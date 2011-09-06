418af39955936ea64218a632dcf2b41b59dad029
Didier Prophete <dprophete@salesforce.com>
2011-09-06 16:43:01 -0700

    added file

---
+++ rebuild_all.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# This script will recompile everything and update all the dependencies of all the projets.
+# After running this script all the .a, .h in all the dependencies directory will be updated
+# with the latest and greatest.
+
+CURRENT_DIR=`pwd`
+
+# first, clean everything
+cd $CURRENT_DIR/json-framework/sfdc_build
+ant clean
+cd $CURRENT_DIR/RestKit/sfdc_build
+ant clean
+cd $CURRENT_DIR/native/SalesforceOAuth/sfdc_build
+ant clean
+
+# now clean libraries with dependencies
+cd $CURRENT_DIR/native/SalesforceSDK/sfdc_build
+ant clean.full
+cd $CURRENT_DIR/native/SampleApp/RestAPIExplorer/sfdc_build
+ant clean.full
+cd $CURRENT_DIR/hybrid/SampleApp/ContactExplorer/sfdc_build
+ant clean.full
+
+# now rebuild everything, starting from the low level libraries
+cd $CURRENT_DIR/json-framework/sfdc_build
+ant
+cd $CURRENT_DIR/RestKit/sfdc_build
+ant
+cd $CURRENT_DIR/native/SalesforceOAuth/sfdc_build
+ant
+
+# now build libraries with dependencies
+cd $CURRENT_DIR/native/SalesforceSDK/sfdc_build
+ant
+cd $CURRENT_DIR/native/SampleApp/RestAPIExplorer/sfdc_build
+ant
+cd $CURRENT_DIR/hybrid/SampleApp/ContactExplorer/sfdc_build
+ant
