6723fe3c70661c58579d789a737cff730de45a08
Todd Stellanova <tstellanova@salesforce.com>
2011-09-28 09:25:42 -0700

    Setup a default Remote Access object on sandbox, make that the default login for RestExplorer and ContactsExplorer

--- hybrid/SampleApps/ContactExplorer/ContactExplorer/Classes/AppDelegate.m
+++ hybrid/SampleApps/ContactExplorer/ContactExplorer/Classes/AppDelegate.m
@@ -1,10 +1,26 @@
-//
-//  AppDelegate.m
-//  ContactExplorer
-//
-//  Created by Didier Prophete on 8/1/11.
-//  Copyright Salesforce.com 2011. All rights reserved.
-//
+/*
+ Copyright (c) 2011, salesforce.com, inc. All rights reserved.
+ 
+ Redistribution and use of this software in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this list of conditions
+ and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of
+ conditions and the following disclaimer in the documentation and/or other materials provided
+ with the distribution.
+ * Neither the name of salesforce.com, inc. nor the names of its contributors may be used to
+ endorse or promote products derived from this software without specific prior written
+ permission of salesforce.com, inc.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #import "AppDelegate.h"
 #ifdef PHONEGAP_FRAMEWORK
@@ -16,9 +32,26 @@
 #import "SFOAuthCredentials.h"
 #import "SFRestAPI.h"
 
-static NSString *const remoteAccessConsumerKey = @"3MVG99OxTyEMCQ3jIW9bdxrL5aAIBz8a993UAC3dntUFefeCE.FJeLrZ.Tt.vcR4USTTa2_H3EGJ6Ajt4dFOw";
-static NSString *const OAuthRedirectURI = @"https://login.salesforce.com/services/oauth2/success";
-static NSString *const OAuthLoginDomain = @"login.salesforce.com";
+
+/*
+ NOTE These values are provided as usable examples to get you started with OAuth login;
+ however, when you create your own app you must create your own Remote Access object
+ in your Salesforce org. 
+ (When you are logged in as an org administrator, go to Setup -> Develop -> Remote Access -> New )
+ */
+
+#warning This value should be overwritten with the Consumer Key from your own Remote Access object
+static NSString *const remoteAccessConsumerKey =
+    @"3MVG9Iu66FKeHhINkB1l7xt7kR8czFcCTUhgoA8Ol2Ltf1eYHOU4SqQRSEitYFDUpqRWcoQ2.dBv_a1Dyu5xa";
+
+#warning This value should be overwritten with the Callback URL from your own Remote Access object
+static NSString *const OAuthRedirectURI = 
+    @"testsfdc:///mobilesdk/detect/oauth/done";
+
+#warning This value must match the org instance with which you're testing 
+static NSString *const OAuthLoginDomain =  
+    @"test.salesforce.com"; //Sandbox:  use login.salesforce.com if you're sure you want to test with Production
+
 
 @interface AppDelegate (private)
 - (void)login;
