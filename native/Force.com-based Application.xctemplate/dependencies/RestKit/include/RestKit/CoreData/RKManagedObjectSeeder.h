7f604f5ce8faa074f261c9ef5cb8b23b3322affb
Todd Stellanova <tstellanova@salesforce.com>
2011-09-30 12:42:27 -0700

    update to latest versions of RestKit and sbjson-framework, rebuild libraries based on latest versions

--- native/Force.com-based Application.xctemplate/dependencies/RestKit/include/RestKit/CoreData/RKManagedObjectSeeder.h
+++ native/Force.com-based Application.xctemplate/dependencies/RestKit/include/RestKit/CoreData/RKManagedObjectSeeder.h
@@ -3,7 +3,19 @@
 //  RestKit
 //
 //  Created by Blake Watters on 3/4/10.
-//  Copyright 2010 Two Toasters. All rights reserved.
+//  Copyright 2010 Two Toasters
+//  
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  
+//  http://www.apache.org/licenses/LICENSE-2.0
+//  
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 #import "../ObjectMapping/ObjectMapping.h"
@@ -57,6 +69,11 @@ extern NSString* const RKDefaultSeedDatabaseFileName;
  * Seed the database with objects from the specified file using the supplied object mapping.
  */
 - (void)seedObjectsFromFile:(NSString*)fileName withObjectMapping:(RKObjectMapping*)nilOrObjectMapping;
+
+/**
+ * Seed the database with objects from the specified file, from the specified bundle, using the supplied object mapping.
+ */
+- (void)seedObjectsFromFile:(NSString *)fileName withObjectMapping:(RKObjectMapping *)nilOrObjectMapping bundle:(NSBundle *)nilOrBundle;
 
 /**
  * Completes a seeding session by persisting the store, outputing an informational message
