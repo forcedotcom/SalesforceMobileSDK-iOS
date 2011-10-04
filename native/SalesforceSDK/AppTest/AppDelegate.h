868b1a2c8e0919aca35a8810d1524e8f5e9741eb
Todd Stellanova <tstellanova@salesforce.com>
2011-10-04 13:16:29 -0700

    Add apptest target

---
+++ native/SalesforceSDK/AppTest/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  AppTest
+//
+//  Created by Todd Stellanova on 10/4/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ native/SalesforceSDK/AppTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AppTest
+//
+//  Created by Todd Stellanova on 10/4/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
