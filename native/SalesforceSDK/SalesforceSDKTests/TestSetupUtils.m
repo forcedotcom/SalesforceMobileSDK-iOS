85907b4a039100cfe09f27a3cc25dee10f60079b
Todd Stellanova <tstellanova@salesforce.com>
2011-10-19 12:32:41 -0700

    Initial support for code coverage on SalesforceSDKTests
    add libprofile_rt.dylib
    set proper code coverage flags on SalesforceSDKTests target
    add gcovr to toolset
    add scripts to collect coverage file output and generate report using gcovr

--- native/SalesforceSDK/SalesforceSDKTests/TestSetupUtils.m
+++ native/SalesforceSDK/SalesforceSDKTests/TestSetupUtils.m
@@ -91,3 +91,15 @@
 }
 
 @end
+
+
+//The following are required for code coverage to work:
+FILE *fopen$UNIX2003(const char *filename, const char *mode) {
+    NSString *covFile = [NSString stringWithCString:filename encoding:NSUTF8StringEncoding];
+    NSLog(@"saving coverage file: %@",covFile);
+    return fopen(filename, mode);
+}
+
+size_t fwrite$UNIX2003(const void *a, size_t b, size_t c, FILE *d) {
+    return fwrite(a, b, c, d);
+}
