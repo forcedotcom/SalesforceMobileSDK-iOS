836ee820cdf8934d3671b5c27162d7133afb506b
Todd Stellanova <tstellanova@salesforce.com>
2011-11-17 14:12:34 -0800

    initial transition over to new hybrid template

--- hybrid/SampleApps/ContactExplorer/ContactExplorer/Classes/AppDelegate.h
+++ hybrid/SampleApps/ContactExplorer/ContactExplorer/Classes/AppDelegate.h
@@ -22,31 +22,14 @@
  WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#import <UIKit/UIKit.h>
-#ifdef PHONEGAP_FRAMEWORK
-	#import <PhoneGap/PhoneGapDelegate.h>
-#else
-	#import "PhoneGapDelegate.h"
-#endif
 
-#import "SFOAuthCoordinator.h"
 
-@interface AppDelegate : PhoneGapDelegate <SFOAuthCoordinatorDelegate, UIAlertViewDelegate> {
+#import "SFContainerAppDelegate.h"
 
-	NSString* invokeString;
-    SFOAuthCoordinator *_coordinator;
+@interface AppDelegate : SFContainerAppDelegate  {
+
 }
 
-// invoke string is passed to your app on launch, this is only valid if you 
-// edit ContactExplorer.plist to add a protocol
-// a simple tutorial can be found here : 
-// http://iphonedevelopertips.com/cocoa/launching-your-own-application-via-a-custom-url-scheme.html
-
-@property (copy)  NSString* invokeString;
-
-@property (nonatomic, retain) SFOAuthCoordinator *coordinator;
-
-+ (BOOL) isIPad;
 
 @end
 
