64278769b99cbc8f6ed3b8322c0aa9de4c2e74dc
Todd Stellanova <tstellanova@salesforce.com>
2012-03-06 11:13:59 -0800

    oauth signature change, location change

--- native/SampleApps/RestAPIExplorer/RestAPIExplorer/NativeRestApp/SFNativeRestAppDelegate.m
+++ native/SampleApps/RestAPIExplorer/RestAPIExplorer/NativeRestApp/SFNativeRestAppDelegate.m
@@ -194,7 +194,9 @@ NSString * const kDefaultLoginHost = @"login.salesforce.com";
         
         SFOAuthCredentials *creds = [[SFOAuthCredentials alloc] 
                                      initWithIdentifier:fullKeychainIdentifier  
-                                     clientId: [self remoteAccessConsumerKey] ];
+                                     clientId: [self remoteAccessConsumerKey] 
+                                     encrypted:YES
+                                     ];
         
         
         creds.domain = loginDomain;
