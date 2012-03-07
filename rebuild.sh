ceba6104c4211294708786514cb84a4fcc887d47
Todd Stellanova <tstellanova@salesforce.com>
2012-03-07 10:32:17 -0800

    move reset.sh to rebuild.sh and do a full rebuild of all binaries

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# The goal here is to clean all binaries so that they will be rebuilt
+# The goal here is to clean all binaries
+# and to rebuild the main SDK binaries and dependencies
 
 CURRENT_DIR=`pwd`
 
@@ -28,6 +29,13 @@ ant clean
 cd $CURRENT_DIR/hybrid/SampleApps/VFConnector/sfdc_build
 ant clean
 
+echo "Rebuilding OAuth library..."
+cd $CURRENT_DIR/native/SalesforceOAuth/sfdc_build
+ant clean all
+
+echo "Rebuilding SalesforceSDK..."
+cd $CURRENT_DIR/native/SalesforceSDK/sfdc_build
+ant clean buildDist
 
 echo "Building and installing Hybrid SDK..."
 cd $CURRENT_DIR/hybrid/sfdc_build
@@ -37,3 +45,11 @@ echo "Building and installing Native SDK..."
 cd $CURRENT_DIR/native/sfdc_build
 ant install
 
+# build sample apps with dependencies
+echo "Building sample apps with dependencies..."
+cd $CURRENT_DIR/native/SampleApps/RestAPIExplorer/sfdc_build
+ant
+cd $CURRENT_DIR/hybrid/SampleApps/ContactExplorer/sfdc_build
+ant
+cd $CURRENT_DIR/hybrid/SampleApps/VFConnector/sfdc_build
+ant
