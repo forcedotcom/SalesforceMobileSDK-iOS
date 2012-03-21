7c6496ddc903a8fbd54131950ba096ba1b340a53
Jonathan Hersh <jon@her.sh>
2012-03-21 16:49:00 -0700

    shiny new SFQueryBuilder utility class!

---
+++ native/SalesforceSDK/SalesforceSDK/Classes/SFQueryBuilder.h
@@ -0,0 +1,120 @@
+/* 
+ * Copyright (c) 2012, salesforce.com, inc.
+ * Author: Jonathan Hersh jhersh@salesforce.com
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided 
+ * that the following conditions are met:
+ * 
+ *    Redistributions of source code must retain the above copyright notice, this list of conditions and the 
+ *    following disclaimer.
+ *  
+ *    Redistributions in binary form must reproduce the above copyright notice, this list of conditions and 
+ *    the following disclaimer in the documentation and/or other materials provided with the distribution. 
+ *    
+ *    Neither the name of salesforce.com, inc. nor the names of its contributors may be used to endorse or 
+ *    promote products derived from this software without specific prior written permission.
+ *  
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED 
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR 
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+ 
+ 
+ /** This utility class assists with creating SOQL and SOSL queries. 
+ 
+ Example SOQL usage:
+ 
+ NSString *soqlQuery = 
+ [SFQueryBuilder SOQLQueryWithFields:[NSArray arrayWithObjects:@"Id", @"Name", @"Company", @"Status", nil]
+							 sObject:@"Lead"
+   							   where:nil
+   							   limit:10];
+   							   
+   							   
+ Example SOSL usage:
+ 
+ NSString *soslQuery = 
+ [SFQueryBuilder SOSLSearchWithSearchTerm:@"hersh"
+							   fieldScope:nil // searches only name fields
+							  objectScope:[NSDictionary dictionaryWithObject:@"WHERE isactive=true and "
+								 		    "( usertype = 'Standard' or usertype = 'CSNOnly' ) ORDER BY lastname asc limit 5"
+								 									  forKey:@"User"]];
+ 
+ */
+
+// Reserved characters that must be escaped in SOSL search terms
+// backslash goes first!
+#define kSOSLReservedCharacters @"\\?&|!{}[]()^~*:\"'+-"
+#define kSOSLEscapeCharacter    @"\\"
+
+// Maximum number of records returned via SOSL search
+#define kMaxSOSLSearchLimit     200
+
+@interface SFQueryBuilder : NSObject {}
+
+/* Sanitizing */
+
++ (NSString *) sanitizeSOSLSearchTerm:(NSString *)searchTerm;
++ (NSString *) sanitizeSOQLQueryFieldList:(NSString *)fieldList;
+
+/* Generating queries */
+
+/**
+ * Generate a SOSL query.
+ * @param term - the search term. This is sanitized for proper characters
+ * @param fieldscope - the SOSL scope, e.g. "IN ALL FIELDS". Defaults to "IN NAME FIELDS" if nil
+ * @param objectScope - nil to search all objects, or a dictionary with keys sObjectName and values of field term to search on each object
+ */
++ (NSString *) SOSLSearchWithSearchTerm:(NSString *)term 
+							 fieldScope:(NSString *)fieldScope 
+						    objectScope:(NSDictionary *)objectScope;
+
+/**
+* Generate a SOSL query.
+* @param term - the search term. This is sanitized for proper characters
+* @param fieldscope - the SOSL scope, e.g. "IN ALL FIELDS". Defaults to "IN NAME FIELDS" if nil
+* @param objectScope - nil to search all objects, or a dictionary with keys sObjectName and values of field term to search on each object
+* @param limit - overall search limit (max 200)
+*/
++ (NSString *) SOSLSearchWithSearchTerm:(NSString *)term 
+							 fieldScope:(NSString *)fieldScope 
+						    objectScope:(NSDictionary *)objectScope 
+							  	  limit:(NSInteger)limit;
+
+/**
+ * Generate a SOQL query.
+ * @param fields - NSArray of fields to select
+ * @param object - object to query
+ * @param where - where clause
+ * @param limit - limit count, or 0 for no limit (for use with query locators)
+ */
++ (NSString *) SOQLQueryWithFields:(NSArray *)fields 
+						   sObject:(NSString *)sObject 
+							 where:(NSString *)where 
+							 limit:(NSUInteger)limit;
+
+/**
+* Generate a SOQL query.
+* @param fields - NSArray of fields to select
+* @param object - object to query
+* @param where - where clause
+* @param groupBy - NSArray of strings, each string is an individual group by clause
+* @param having - having clause
+* @param orderBy - NSArray of strings, each string is an individual order by clause
+* @param limit - limit count, or 0 for no limit (for use with query locators)
+*/
++ (NSString *) SOQLQueryWithFields:(NSArray *)fields 
+						   sObject:(NSString *)sObject 
+							 where:(NSString *)where 
+						   groupBy:(NSArray *)groupBy 
+							having:(NSString *)having
+						   orderBy:(NSArray *)orderBy 
+							 limit:(NSUInteger)limit;
+
+@end
