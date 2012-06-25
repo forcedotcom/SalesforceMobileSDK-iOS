dcd1b71b20dfd46a08ad2748668b7e96d0b5d0dc
Kevin Hawkins <khawkins@salesforce.com>
2012-06-25 09:50:45 -0700

    Updating to 1.2.2

--- hybrid/SalesforceHybridSDK/SalesforceHybridSDK/Container/SFContainerAppDelegate.m
+++ hybrid/SalesforceHybridSDK/SalesforceHybridSDK/Container/SFContainerAppDelegate.m
@@ -34,7 +34,7 @@
 #import "SFSmartStore.h"
 
 // Public constants
-NSString * const kSFMobileSDKVersion = @"1.2.1";
+NSString * const kSFMobileSDKVersion = @"1.2.2";
 NSString * const kUserAgentPropKey = @"UserAgent";
 NSString * const kAppHomeUrlPropKey = @"AppHomeUrl";
 NSString * const kSFMobileSDKHybridDesignator = @"Hybrid";
