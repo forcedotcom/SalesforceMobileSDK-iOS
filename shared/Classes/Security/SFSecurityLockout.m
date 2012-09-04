3343005fa21a730edb611379bde7eb59b7787ce0
Kevin Hawkins <khawkins@salesforce.com>
2012-09-03 17:24:20 -0700

    Tests for passcode changes

--- shared/Classes/Security/SFSecurityLockout.m
+++ shared/Classes/Security/SFSecurityLockout.m
@@ -23,6 +23,7 @@
  */
 
 #import "SFSecurityLockout.h"
+#import "SFSecurityLockout+Internal.h"
 #import "SFInactivityTimerCenter.h"
 #import "SFPasscodeViewController.h"
 #import "SFOAuthCredentials.h"
@@ -52,51 +53,6 @@ static SFLockScreenCallbackBlock sLockScreenFailureCallbackBlock = NULL;
 // Flag used to prevent the display of the passcode view controller.
 // Note: it is used by the unit tests only.
 static BOOL _showPasscode = YES;
-
-@interface SFSecurityLockout () 
-
-/**
- * Called when the user activity timer expires.
- */
-+ (void)timerExpired:(NSTimer *)theTimer;
-
-/**
- * Presents the passcode controller when it's time to create or verify the passcode.
- */
-+ (void)presentPasscodeController:(SFPasscodeControllerMode)modeValue;
-
-/**
- * Sets a retained instance of the current passcode view controller that's displayed.
- */
-+ (void)setPasscodeViewController:(UIViewController *)vc;
-
-/**
- * Returns the currently displayed passcode view controller, or nil if the passcode view controller
- * is not currently displayed.
- */
-+ (UIViewController *)passcodeViewController;
-
-/**
- * Whether or not the passcode screen is currently displayed.
- */
-+ (BOOL)passcodeScreenIsPresent;
-
-/**
- * Whether or not the app currently has a valid authenticated session.
- */
-+ (BOOL)hasValidSession;
-
-/**
- * Runs in the event of a successful passcode unlock.
- */
-+ (void)unlockSuccessPostProcessing;
-
-/**
- * Runs in the event that a passcode unlock attempt failed.
- */
-+ (void)unlockFailurePostProcessing;
-
-@end
 
 @implementation SFSecurityLockout
 
@@ -169,6 +125,12 @@ static BOOL _showPasscode = YES;
         }
 	}
     [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
+// For unit tests.
++ (void)setLockoutTimeInternal:(NSUInteger)seconds
+{
+    securityLockoutTime = seconds;
 }
 
 + (NSUInteger)lockoutTime {
