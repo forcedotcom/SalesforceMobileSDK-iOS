ade8cafdfe6024e3a061a77f5d7d804e2e63dce0
Kevin Hawkins <khawkins@salesforce.com>
2013-04-21 19:03:01 -0700

    Account Edtitor sample app

---
+++ hybrid/SampleApps/AccountEditor/AccountEditor/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  AccountEditor
+//
+//  Created by Kevin Hawkins on 4/19/13.
+//  Copyright (c) 2013 salesforce.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+#import "SFApplication.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, NSStringFromClass([SFApplication class]), NSStringFromClass([AppDelegate class]));
+    }
+}
