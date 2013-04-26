c267b181a688721d066bc31c9d378a1162ee1f30
Kevin Hawkins <khawkins@salesforce.com>
2013-04-26 10:22:00 -0700

    CWD changes are local to the script

--- install.sh
+++ install.sh
@@ -4,12 +4,11 @@
 # Run this script before working with the SalesforceMobileSDK Xcode workspace.
 #
 
-ORIG_CWD=`pwd`
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$SCRIPT_DIR"
 git submodule init
 git submodule sync
 git submodule update 
-cd "$ORIG_CWD"
+
 
