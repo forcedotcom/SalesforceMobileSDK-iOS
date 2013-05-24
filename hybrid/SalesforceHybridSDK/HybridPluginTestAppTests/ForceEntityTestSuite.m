f3e50fdf9299991e201ffecbb8012a18d5ee4c8e
Wolfgang Mathurin <wmathurin@salesforce.com>
2013-05-24 14:36:50 -0700

    JS entity framework tests now working inside container

--- hybrid/SalesforceHybridSDK/HybridPluginTestAppTests/ForceEntityTestSuite.m
+++ hybrid/SalesforceHybridSDK/HybridPluginTestAppTests/ForceEntityTestSuite.m
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2012, salesforce.com, inc. All rights reserved.
+ Copyright (c) 2013, salesforce.com, inc. All rights reserved.
  
  Redistribution and use of this software in source and binary forms, with or without modification,
  are permitted provided that the following conditions are met:
@@ -26,12 +26,34 @@
 
 #import "ForceEntityTestSuite.h"
 
+#import "AppDelegate.h"
+#import "SFTestRunnerPlugin.h"
+#import "SFSmartStore.h"
+#import "SFSmartStorePlugin.h"
+#import "SFHybridViewController.h"
+#import "CDVCommandDelegateImpl.h"
 
 @implementation ForceEntityTestSuite
 
-- (void)setUp {
+- (void)setUp
+{
     [super setUp];
     self.jsSuiteName = @"ForceEntityTestSuite";
+    
+    if ([self isTestRunnerReady]) {
+        [SFSmartStore removeSharedStoreWithName:kDefaultSmartStoreName];
+        AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
+        SFSmartStorePlugin *pluginInstance = [appDelegate.viewController.commandDelegate getCommandInstance:kSmartStorePluginIdentifier];
+        [pluginInstance resetSharedStore];
+    }
+    
+}
+
+- (void)tearDown
+{
+    [SFSmartStore removeSharedStoreWithName:kDefaultSmartStoreName];
+    
+    [super tearDown];
 }
 
 
