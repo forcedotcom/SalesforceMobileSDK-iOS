6354485a97783198972f167967da0add97e848a4
Kevin Hawkins <khawkins@salesforce.com>
2013-12-18 16:56:18 -0800

    Passcode settings security updates

---
+++ shared/SalesforceSDKCore/SalesforceSDKCoreTests/SFSecurityLockoutTests.h
@@ -0,0 +1,13 @@
+//
+//  SFSecurityLockoutTests.h
+//  SalesforceSDKCore
+//
+//  Created by Kevin Hawkins on 12/17/13.
+//  Copyright (c) 2013 salesforce.com. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SFSecurityLockoutTests : SenTestCase
+
+@end
