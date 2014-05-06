f9abdf0f47fdfdab7a3b8b587be69f79258eb4ca
Wolfgang Mathurin <wmathurin@salesforce.com>
2014-05-05 17:26:55 -0700

    SFAlterSoupLongOperation: boiler plate code only

---
+++ shared/SalesforceSDKCore/SalesforceSDKCore/Classes/SmartStore/SFAlterSoupLongOperation.m
@@ -0,0 +1,75 @@
+/*
+ Copyright (c) 2014, salesforce.com, inc. All rights reserved.
+ 
+ Redistribution and use of this software in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this list of conditions
+ and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of
+ conditions and the following disclaimer in the documentation and/or other materials provided
+ with the distribution.
+ * Neither the name of salesforce.com, inc. nor the names of its contributors may be used to
+ endorse or promote products derived from this software without specific prior written
+ permission of salesforce.com, inc.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import "SFAlterSoupLongOperation.h"
+
+@implementation SFAlterSoupLongOperation
+
+@synthesize store = _store;
+@synthesize db = _storeDb;
+@synthesize soupName = _soupName;
+@synthesize soupTableName = _soupTableName;
+@synthesize reIndexData = _reIndexData;
+@synthesize rowId = _rowId;
+@synthesize indexSpecs = _indexSpecs;
+@synthesize oldIndexSpecs = _oldIndexSpecs;
+@synthesize afterStep = _afterStep;
+
+
+- (id) init:(SFSmartStore*)store withSoupName:(NSString*)soupName withNewIndexSpecs:(NSArray*)newIndexSpecs withReIndexData:(BOOL)reIndexData
+{
+    self = [super init];
+    if (nil != self) {
+        _store = store;
+        _db = store.storeDb;
+        _soupName = soupName;
+        _indexSpecs = newIndexSpecs;
+        _reIndexData = reIndexData;
+    }
+    return self;
+}
+
+- (id) init:(SFSmartStore*) store withRowId:(long) rowId withDetails:(NSDictionary*)details withStatus:(int)status
+{
+    self = [super init];
+    if (nil != self) {
+        _store = store;
+        _db = store.storeDb;
+        // TODO extra from details
+        _afterStep = status;
+    }
+    return self;
+}
+
+- (void) run
+{
+    [self run:kLastStep];
+}
+
+- (void) run:(SFAlterSoupStep) toStep
+{
+    // TBD
+}
+
+@end
