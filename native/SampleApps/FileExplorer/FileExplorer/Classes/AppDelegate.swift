2c16dabf2c10d44aac9342f8854e89c45ff5e24c
Wolfgang Mathurin <wmathurin@salesforce.com>
2014-07-28 17:58:10 -0700

    Switify (almost all of) AppDelegate for FileExplorer

---
+++ native/SampleApps/FileExplorer/FileExplorer/Classes/AppDelegate.swift
@@ -0,0 +1,152 @@
+//
+//  AppDelegate.swift
+//  FileExplorer
+//
+//  Created by Wolfgang Mathurin on 7/28/14.
+//  Copyright (c) 2014 salesforce. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// Fill these in when creating a new Connected Application on Force.com
+let RemoteAccessConsumerKey = "3MVG9Iu66FKeHhINkB1l7xt7kR8czFcCTUhgoA8Ol2Ltf1eYHOU4SqQRSEitYFDUpqRWcoQ2.dBv_a1Dyu5xa";
+let OAuthRedirectURI        = "testsfdc:///mobilesdk/detect/oauth/done";
+let scopes = ["api"];
+
+@UIApplicationMain
+class AppDelegate : UIResponder, UIApplicationDelegate, SFAuthenticationManagerDelegate, SFUserAccountManagerDelegate
+{
+    var window: UIWindow?
+    var initialLoginSuccessBlock: SFOAuthFlowSuccessCallbackBlock?
+    var initialLoginFailureBlock: SFOAuthFlowFailureCallbackBlock?
+    
+    init()
+    {
+        super.init()
+        SFLogger.setLogLevel(SFLogLevelDebug)
+        
+        // These SFAccountManager settings are the minimum required to identify the Connected App.
+        SFUserAccountManager.sharedInstance().oauthClientId = RemoteAccessConsumerKey
+        SFUserAccountManager.sharedInstance().oauthCompletionUrl = OAuthRedirectURI
+        SFUserAccountManager.sharedInstance().scopes = NSSet(array:scopes)
+        
+        // Auth manager delegate, for receiving logout and login host change events.
+        SFAuthenticationManager.sharedManager().addDelegate(self)
+        SFUserAccountManager.sharedInstance().addDelegate(self)
+        
+        // Blocks to execute once authentication has completed.  You could define these at the different boundaries where
+        // authentication is initiated, if you have specific logic for each case.
+        self.initialLoginSuccessBlock = {
+            [weak self] info in
+            self!.setupRootViewController()
+        }
+        
+        self.initialLoginFailureBlock = {
+            info, error in
+            SFAuthenticationManager.sharedManager().logout()
+        }
+    }
+
+    deinit
+    {
+        SFAuthenticationManager.sharedManager().removeDelegate(self)
+        SFUserAccountManager.sharedInstance().removeDelegate(self)
+    }
+    
+    // MARK: - App delegate lifecycle
+    
+    func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]!) -> Bool
+    {
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.initializeAppViewState();
+        
+        //
+        // If you wish to register for push notifications, uncomment the line below.  Note that,
+        // if you want to receive push notifications from Salesforce, you will also need to
+        // implement the application:didRegisterForRemoteNotificationsWithDeviceToken: method (below).
+        //
+        // SFPushNotificationManager.sharedInstance().registerForRemoteNotifications()
+        
+        SFAuthenticationManager.sharedManager().loginWithCompletion(self.initialLoginSuccessBlock, failure: self.initialLoginFailureBlock)
+        
+        return true
+    }
+    
+    func application(application: UIApplication!, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData!)
+    {
+        //
+        // Uncomment the code below to register your device token with the push notification manager
+        //
+        //
+        // SFPushNotificationManager.sharedInstance().didRegisterForRemoteNotificationsWithDeviceToken(deviceToken)
+        // if (SFAccountManager.sharedInstance().credentials.accessToken != nil)
+        // {
+        //    SFPushNotificationManager.sharedInstance().registerForSalesforceNotifications()
+        // }
+    }
+    
+    
+    func application(application: UIApplication!, didFailToRegisterForRemoteNotificationsWithError error: NSError!)
+    {
+        // Respond to any push notification registration errors here.
+    }
+
+    // MARK: - Private methods
+    func initializeAppViewState()
+    {
+        self.window!.rootViewController = InitialViewController(nibName: nil, bundle: nil)
+        self.window!.makeKeyAndVisible()
+    }
+    
+    func setupRootViewController()
+    {
+        var rootVC = RootViewController(nibName: nil, bundle: nil)
+        var navVC = UINavigationController(rootViewController: rootVC)
+        self.window!.rootViewController = navVC
+    }
+    
+    // MARK: - SFAuthenticationManagerDelegate
+    func authManagerDidLogout(manager: SFAuthenticationManager!)
+    {
+        self.log(SFLogLevelDebug, msg: "SFAuthenticationManagerDelegate")
+        self.initializeAppViewState()
+        
+        // Multi-user pattern:
+        // - If there are two or more existing accounts after logout, let the user choose the account
+        //   to switch to.
+        // - If there is one existing account, automatically switch to that account.
+        // - If there are no further authenticated accounts, present the login screen.
+        //
+        // Alternatively, you could just go straight to re-initializing your app state, if you know
+        // your app does not support multiple accounts.  The logic below will work either way.
+        
+        /* FIXME
+        NSArray *allAccounts = [SFUserAccountManager sharedInstance].allUserAccounts;
+        if ([allAccounts count] > 1) {
+            SFDefaultUserManagementViewController *userSwitchVc = [[SFDefaultUserManagementViewController alloc] initWithCompletionBlock:^(SFUserManagementAction action) {
+                [self.window.rootViewController dismissViewControllerAnimated:YES completion:NULL];
+                }];
+            [self.window.rootViewController presentViewController:userSwitchVc animated:YES completion:NULL];
+        } else if ([[SFUserAccountManager sharedInstance].allUserAccounts count] == 1) {
+            [SFUserAccountManager sharedInstance].currentUser = ([SFUserAccountManager sharedInstance].allUserAccounts)[0];
+            [[SFAuthenticationManager sharedManager] loginWithCompletion:self.initialLoginSuccessBlock
+            failure:self.initialLoginFailureBlock];
+        } else {
+            [[SFAuthenticationManager sharedManager] loginWithCompletion:self.initialLoginSuccessBlock
+                failure:self.initialLoginFailureBlock];
+        */
+    }
+    
+    // MARK: - SFUserAccountManagerDelegate
+    func userAccountManager(userAccountManager: SFUserAccountManager!, didSwitchFromUser fromUser: SFUserAccount!, toUser: SFUserAccount!)
+    {
+        /* FIXME
+        [self log:SFLogLevelDebug format:@"SFUserAccountManager changed from user %@ to %@.  Resetting app.",
+        fromUser.userName, toUser.userName];
+        [self initializeAppViewState];
+        [[SFAuthenticationManager sharedManager] loginWithCompletion:self.initialLoginSuccessBlock failure:self.initialLoginFailureBlock];
+        */
+        
+    }
+}
--- native/SampleApps/FileExplorer/FileExplorer/Classes/FileExplorer-Bridging-Header.h
+++ native/SampleApps/FileExplorer/FileExplorer/Classes/FileExplorer-Bridging-Header.h
@@ -2,3 +2,10 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#import "RootViewController.h"
+#import <SalesforceCommonUtils/SFLogger.h>
+#import <SalesforceSDKCore/SFUserAccountManager.h>
+#import <SalesforceSDKCore/SFAuthenticationManager.h>
+#import <SalesforceSDKCore/SFPushNotificationManager.h>
+#import <SalesforceSDKCore/SFDefaultUserManagementViewController.h>
+#import <SalesforceOAuth/SFOAuthInfo.h>
