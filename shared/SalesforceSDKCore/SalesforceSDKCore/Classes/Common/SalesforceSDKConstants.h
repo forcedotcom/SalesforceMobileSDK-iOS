71f98b49a009754c0c15e3339643b42c30487d8a
Kevin Hawkins <khawkins@salesforce.com>
2014-08-27 16:55:58 -0700

    Merging master back to unstable

--- shared/SalesforceSDKCore/SalesforceSDKCore/Classes/Common/SalesforceSDKConstants.h
+++ shared/SalesforceSDKCore/SalesforceSDKCore/Classes/Common/SalesforceSDKConstants.h
@@ -1,7 +1,7 @@
 #ifndef SalesforceSDKConstants_h
 #define SalesforceSDKConstants_h
 
-#define SALESFORCE_SDK_IS_PRODUCTION_VERSION YES
+#define SALESFORCE_SDK_IS_PRODUCTION_VERSION NO
 
 #define SALESFORCE_SDK_BUILD_IDENTIFIER @".unstable"
 
@@ -19,8 +19,9 @@
 #define __SALESFORCE_SDK_2_2_1 20201
 #define __SALESFORCE_SDK_2_2_2 20202
 #define __SALESFORCE_SDK_2_3_0 20300
+#define __SALESFORCE_SDK_3_0_0 30000
 
-#define SALESFORCE_SDK_VERSION_MIN_REQUIRED __SALESFORCE_SDK_2_3_0
+#define SALESFORCE_SDK_VERSION_MIN_REQUIRED __SALESFORCE_SDK_3_0_0
 
 #define SALESFORCE_SDK_VERSION [NSString stringWithFormat:@"%d.%d.%d%@",              \
                                 (SALESFORCE_SDK_VERSION_MIN_REQUIRED / 10000),        \
