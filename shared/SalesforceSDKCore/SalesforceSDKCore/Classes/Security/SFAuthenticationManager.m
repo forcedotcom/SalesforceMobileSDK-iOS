0f2433b3da127036eba6c1ee22d77a31013228b4
chris.greening <chris@cmgresearch.com>
2014-08-28 09:35:35 +0100

    Fixes error alertview so it shows friendly message

--- shared/SalesforceSDKCore/SalesforceSDKCore/Classes/Security/SFAuthenticationManager.m
+++ shared/SalesforceSDKCore/SalesforceSDKCore/Classes/Security/SFAuthenticationManager.m
@@ -1061,7 +1061,7 @@ static Class InstanceClass = nil;
     if (nil == _statusAlert) {
         // show alert and allow retry
         _statusAlert = [[UIAlertView alloc] initWithTitle:[SFSDKResourceUtils localizedString:kAlertErrorTitleKey]
-                                                  message:[NSString stringWithFormat:[SFSDKResourceUtils localizedString:kAlertConnectionErrorFormatStringKey], error]
+                                                  message:[NSString stringWithFormat:[SFSDKResourceUtils localizedString:kAlertConnectionErrorFormatStringKey], [error localizedDescription]]
                                                  delegate:self
                                         cancelButtonTitle:[SFSDKResourceUtils localizedString:kAlertRetryButtonKey]
                                         otherButtonTitles: nil];
