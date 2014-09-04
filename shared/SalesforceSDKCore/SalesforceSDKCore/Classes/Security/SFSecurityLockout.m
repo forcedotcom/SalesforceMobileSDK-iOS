3570cc81ef43428c1d63f330d74c019893910766
Jean Bovet <jbovet@salesforce.com>
2014-09-03 21:20:45 -0500

    Prevent the user activity monitor from being stopped as it can be used by other classes in the app

--- shared/SalesforceSDKCore/SalesforceSDKCore/Classes/Security/SFSecurityLockout.m
+++ shared/SalesforceSDKCore/SalesforceSDKCore/Classes/Security/SFSecurityLockout.m
@@ -279,7 +279,6 @@ static BOOL _showPasscode = YES;
     [SFSecurityLockout setSecurityLockoutTime:0];
     [SFSecurityLockout setPasscodeLength:kDefaultPasscodeLength];
     [SFInactivityTimerCenter removeTimer:kTimerSecurity];
-    [[SFUserActivityMonitor sharedInstance] stopMonitoring];
     [[SFPasscodeManager sharedManager] changePasscode:nil];
 }
 
