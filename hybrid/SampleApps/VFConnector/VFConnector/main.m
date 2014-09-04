c6750813526b206f8aa9aaed9e55d0d7ef997179
Kevin Hawkins <khawkins@salesforce.com>
2014-09-04 12:42:31 -0700

    Adding passcode-related category to UIApplication

--- hybrid/SampleApps/VFConnector/VFConnector/main.m
+++ hybrid/SampleApps/VFConnector/VFConnector/main.m
@@ -27,7 +27,7 @@
 int main(int argc, char *argv[]) {
     
     @autoreleasepool {
-        int retVal = UIApplicationMain(argc, argv, @"SFApplication", @"AppDelegate");
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
         return retVal;
     }
 }
