5fc9d7e210e0b6e35acf5c9d3020c8163cce2a06
Kevin Hawkins <khawkins@salesforce.com>
2014-09-26 12:48:56 -0700

    Adding SFSDKAppConfig file

---
+++ shared/SalesforceSDKCore/SalesforceSDKCore/Classes/Common/SFSDKAppConfig.h
@@ -0,0 +1,13 @@
+//
+//  SFSDKAppConfig.h
+//  SalesforceSDKCore
+//
+//  Created by Kevin Hawkins on 9/26/14.
+//  Copyright (c) 2014 salesforce.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SFSDKAppConfig : NSObject
+
+@end
---
+++ shared/SalesforceSDKCore/SalesforceSDKCore/Classes/Common/SFSDKAppConfig.m
@@ -0,0 +1,13 @@
+//
+//  SFSDKAppConfig.m
+//  SalesforceSDKCore
+//
+//  Created by Kevin Hawkins on 9/26/14.
+//  Copyright (c) 2014 salesforce.com. All rights reserved.
+//
+
+#import "SFSDKAppConfig.h"
+
+@implementation SFSDKAppConfig
+
+@end
