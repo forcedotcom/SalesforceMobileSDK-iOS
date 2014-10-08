c2756bafe0b13aaad6cfd73540b342d21ce7ba26
Kevin Hawkins <khawkins@salesforce.com>
2014-10-08 16:38:45 -0700

    Adding SmartSyncExplorer sample app project

---
+++ native/SampleApps/SmartSyncExplorer/SmartSyncExplorer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SmartSyncExplorer
+//
+//  Created by Kevin Hawkins on 10/8/14.
+//  Copyright (c) 2014 salesforce.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
