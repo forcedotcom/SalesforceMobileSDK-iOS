c2f239e5cb06b28096411f429549dfb18584c877
Kevin Hawkins <khawkins@salesforce.com>
2014-12-05 11:16:39 -0800

    OAuth unit tests to XCUnit

--- libs/SalesforceOAuth/SalesforceOAuthUnitTests/SalesforceOAuthUnitTests.h
+++ libs/SalesforceOAuth/SalesforceOAuthUnitTests/SalesforceOAuthUnitTests.h
@@ -22,8 +22,8 @@
  WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface SalesforceOAuthUnitTests : SenTestCase
+@interface SalesforceOAuthUnitTests : XCTestCase
 
 @end
--- libs/SalesforceOAuth/SalesforceOAuthUnitTests/SalesforceOAuthUnitTestsCoordinatorDelegate.m
+++ libs/SalesforceOAuth/SalesforceOAuthUnitTests/SalesforceOAuthUnitTestsCoordinatorDelegate.m
@@ -30,25 +30,25 @@
 - (void)oauthCoordinator:(SFOAuthCoordinator *)coordinator willBeginAuthenticationWithView:(UIWebView *)view {
     // we should never be called here as the test sets a refresh token in the credentials, 
     // therefore we do the refresh flow instead of the user agent flow
-    STFail(@"user agent authentication flow should not begin");
+    XCTFail(@"user agent authentication flow should not begin");
 }
 
 - (void)oauthCoordinator:(SFOAuthCoordinator *)coordinator didBeginAuthenticationWithView:(UIWebView *)view {
     // we should never be called here as the test sets a refresh token in the credentials, 
     // therefore we do the refresh flow instead of the user agent flow
-    STFail(@"user agent authentication flow should not begin");
+    XCTFail(@"user agent authentication flow should not begin");
 }
 
 - (void)oauthCoordinatorDidAuthenticate:(SFOAuthCoordinator *)coordinator {
     // authentication is performed against localhost (which presumably has no oauth process listening) 
     // and authentication is immediately cancelled after being started, so the authentication should 
     // never succeed.
-    STFail(@"coordinator test should not be able to authenticate");
+    XCTFail(@"coordinator test should not be able to authenticate");
 }
 
 - (void)oauthCoordinator:(SFOAuthCoordinator *)coordinator didFailWithError:(NSError *)error {
     // authentication is cancelled before a timeout can occur, so the test should not fail
-    STFail(@"user agent authentication flow should not fail");
+    XCTFail(@"user agent authentication flow should not fail");
 }
 
 @end
