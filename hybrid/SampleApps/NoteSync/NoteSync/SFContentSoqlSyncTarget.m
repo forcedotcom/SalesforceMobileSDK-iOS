726d7096a859c87e787890cf6125cb2c976ad473
Wolfgang Mathurin <wmathurin@salesforce.com>
2015-02-18 17:25:11 -0800

    Partial implementation of SFContentSoqlSyncTarget (not working yet)

--- hybrid/SampleApps/NoteSync/NoteSync/SFContentSoqlSyncTarget.m
+++ hybrid/SampleApps/NoteSync/NoteSync/SFContentSoqlSyncTarget.m
@@ -24,6 +24,107 @@
 
 #import "SFContentSoqlSyncTarget.h"
 #import <SmartSync/SFSmartSyncSyncManager.h>
+#import <SmartSync/SFSmartSyncConstants.h>
+
+
+#define REQUEST_TEMPLATE @"<?xml version=\"1.0\"?>"\
+"<se:Envelope xmlns:se=\"http://schemas.xmlsoap.org/soap/envelope/\">"\
+"<se:Header xmlns:sfns=\"urn:partner.soap.sforce.com\">"\
+"<sfns:SessionHeader><sessionId>%@</sessionId></sfns:SessionHeader>"\
+"</se:Header>"\
+"<se:Body>%@</se:Body>"\
+"</se:Envelope>"
+
+#define QUERY_TEMPLATE @"<query xmlns=\"urn:partner.soap.sforce.com\" xmlns:ns1=\"sobject.partner.soap.sforce.com\">"\
+"<queryString>%@</queryString></query>"
+
+
+#define QUERY_MORE_TEMPLATE @"<queryMore xmlns=\"urn:partner.soap.sforce.com\" xmlns:ns1=\"sobject.partner.soap.sforce.com\">"\
+"<queryLocator>%@</queryLocator>"\
+"</queryMore>"
+
+#define SESSION_ID @"sessionId"
+#define QUERY @"query"
+#define IS_LOCATOR @"query"
+#define XML_MIME_TYPE @"text/xml"
+#define SOAP_ACTION @"SOAPAction"
+#define SOAP_ACTION_VALUE @"\"\""
+
+
+@interface SFSoapSoqlRequest : SFRestRequest
+
+@property (nonatomic, strong) NSString *query;
+@property (nonatomic) BOOL isLocator;
+
+- (id)initWithQuery:(NSString*) query;
+- (id)initWithQueryLocator:(NSString*) queryLocator;
+
+@end
+
+@implementation SFSoapSoqlRequest
+
+- (id)initWithQuery:(NSString*)query
+{
+    self = [super init];
+    if (self) {
+        self.method = SFRestMethodPOST;
+        self.path = @"";
+        self.endpoint = @"/services/Soap/u/32.0";
+        self.parseResponse = NO;
+        self.isLocator = NO;
+        self.query = query;
+        
+    }
+    return self;
+}
+
+- (id)initWithQueryLocator:(NSString*)queryLocator
+{
+    self = [super init];
+    if (self) {
+        self.method = SFRestMethodPOST;
+        self.path = @"";
+        self.endpoint = @"/services/Soap/u/32.0";
+        self.parseResponse = NO;
+        self.isLocator = YES;
+        self.query = queryLocator;
+    }
+    return self;
+}
+
+
+- (SFNetworkOperation*) send:(SFNetworkEngine*) networkEngine {
+    NSString *url = [NSString stringWithFormat:@"%@%@", self.endpoint, self.path];
+    // FIXME get session id
+    [networkEngine post:url params:@{ SESSION_ID:@"", QUERY:self.query, IS_LOCATOR:self.isLocator ? @YES : @NO} ];
+    
+    SFNetworkOperationEncodingBlock enncodingBlock = ^(NSDictionary *postDataDict) {
+        NSString* template = postDataDict[IS_LOCATOR] ? QUERY_MORE_TEMPLATE : QUERY_TEMPLATE;
+        return [NSString stringWithFormat:REQUEST_TEMPLATE, postDataDict[SESSION_ID], [NSString stringWithFormat:template, postDataDict[QUERY]]];
+    };
+    [self.networkOperation setCustomPostDataEncodingHandler:enncodingBlock forType:XML_MIME_TYPE];
+
+    // Add any custom headers to the network operation.
+    if (self.customHeaders != nil) {
+        [self.customHeaders enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+            [self.networkOperation setHeaderValue:obj forKey:key];
+        }];
+        [self.networkOperation setHeaderValue:SOAP_ACTION forKey:SOAP_ACTION_VALUE];
+    }
+    
+    self.networkOperation.delegate = self;
+    [networkEngine enqueueOperation:self.networkOperation];
+    
+    return self.networkOperation;
+}
+
+@end
+
+@interface SFContentSoqlSyncTarget ()
+
+@property (nonatomic, strong, readwrite) NSString* queryLocator;
+
+@end
 
 @implementation SFContentSoqlSyncTarget
 
@@ -65,18 +166,42 @@
       completeBlock:(SFSyncTargetFetchCompleteBlock)completeBlock
 {
     
-    // FIXME
+    __weak SFContentSoqlSyncTarget* weakSelf = self;
     
-    completeBlock(nil);
+    // Resync?
+    NSString* queryToRun = self.query;
+    if (maxTimeStamp > 0) {
+        queryToRun = [SFSoqlSyncTarget addFilterForReSync:self.query maxTimeStamp:maxTimeStamp];
+    }
+    
+    SFRestRequest* request = [[SFSoapSoqlRequest alloc] initWithQuery:queryToRun];
+    [syncManager sendRequestWithSmartSyncUserAgent:request failBlock:errorBlock completeBlock:^(id data) {
+        // FIXME
+        // weakSelf.totalSize = [d[kResponseTotalSize] integerValue];
+        // weakSelf.nextRecordsUrl = d[kResponseNextRecordsUrl];
+        // completeBlock(d[kResponseRecords]);
+        completeBlock(nil);
+
+    }];
 }
 
 - (void) continueFetch:(SFSmartSyncSyncManager *)syncManager
             errorBlock:(SFSyncTargetFetchErrorBlock)errorBlock
          completeBlock:(SFSyncTargetFetchCompleteBlock)completeBlock
 {
-    // FIXME
-    
-    completeBlock(nil);
+    if (self.queryLocator) {
+        __weak SFContentSoqlSyncTarget* weakSelf = self;
+        SFRestRequest* request = [[SFSoapSoqlRequest alloc] initWithQueryLocator:self.queryLocator];
+        [syncManager sendRequestWithSmartSyncUserAgent:request failBlock:errorBlock completeBlock:^(id data) {
+            // FIXME
+            // weakSelf.nextRecordsUrl = d[kResponseNextRecordsUrl];
+            // completeBlock(d[kResponseRecords]);
+            completeBlock(nil);
+        }];
+    }
+    else {
+        completeBlock(nil);
+    }
 }
 
 @end
