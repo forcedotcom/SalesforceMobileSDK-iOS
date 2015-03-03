a2b9ee7aa311275b674529d158b7b69d75974469
Wolfgang Mathurin <wmathurin@salesforce.com>
2015-03-03 12:43:35 -0800

    Was missing index on Id

--- hybrid/SampleApps/SimplySyncReact/SimpleSyncReact/js/App.js
+++ hybrid/SampleApps/SimplySyncReact/SimpleSyncReact/js/App.js
@@ -35,6 +35,6 @@ var styles = StyleSheet.create({
 AppRegistry.registerComponent('App', () => App);
 
 // Wrong place?
-SmartStore.registerSoup("users", [ {path:"FirstName", type:"string"}, {path:"LastName", type:"string"} ],  function() {}, function() {});
+SmartStore.registerSoup("users", [ {path:"Id", type:"string"}, {path:"FirstName", type:"string"}, {path:"LastName", type:"string"}, {path:"__local__", type:"string"} ],  function() {}, function() {});
 
 module.exports = App;
