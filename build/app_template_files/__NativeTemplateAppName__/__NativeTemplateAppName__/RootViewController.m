23cfb7b618c4f914fcb04d45f8d9d325725d19c9
Wolfgang Mathurin <wmathurin@salesforce.com>
2015-08-07 14:46:57 -0700

    Minor cleanup in native template

--- build/app_template_files/__NativeTemplateAppName__/__NativeTemplateAppName__/RootViewController.m
+++ build/app_template_files/__NativeTemplateAppName__/__NativeTemplateAppName__/RootViewController.m
@@ -55,7 +55,7 @@
     self.title = @"Mobile SDK Sample App";
     
     //Here we use a query that should work on either Force.com or Database.com
-    SFRestRequest *request = [[SFRestAPI sharedInstance] requestForQuery:@"SELECT Name FROM User LIMIT 10"];    
+    SFRestRequest *request = [[SFRestAPI sharedInstance] requestForQuery:@"SELECT Name FROM User LIMIT 10"];
     [[SFRestAPI sharedInstance] send:request delegate:self];
 }
 
@@ -73,17 +73,17 @@
 
 - (void)request:(SFRestRequest*)request didFailLoadWithError:(NSError*)error {
     NSLog(@"request:didFailLoadWithError: %@", error);
-    //add your failed error handling here
+    // Add your failed error handling here
 }
 
 - (void)requestDidCancelLoad:(SFRestRequest *)request {
     NSLog(@"requestDidCancelLoad: %@", request);
-    //add your failed error handling here
+    // Add your failed error handling here
 }
 
 - (void)requestDidTimeout:(SFRestRequest *)request {
     NSLog(@"requestDidTimeout: %@", request);
-    //add your failed error handling here
+    // Add your failed error handling here
 }
 
 
@@ -99,26 +99,26 @@
 
 // Customize the appearance of table view cells.
 - (UITableViewCell *)tableView:(UITableView *)tableView_ cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-   static NSString *CellIdentifier = @"CellIdentifier";
-
-   // Dequeue or create a cell of the appropriate type.
+    static NSString *CellIdentifier = @"CellIdentifier";
+    
+    // Dequeue or create a cell of the appropriate type.
     UITableViewCell *cell = [tableView_ dequeueReusableCellWithIdentifier:CellIdentifier];
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:CellIdentifier];
-
+        
     }
-	//if you want to add an image to your cell, here's how
-	UIImage *image = [UIImage imageNamed:@"icon.png"];
-	cell.imageView.image = image;
-
-	// Configure the cell to show the data.
-	NSDictionary *obj = [dataRows objectAtIndex:indexPath.row];
-	cell.textLabel.text =  [obj objectForKey:@"Name"];
-
-	//this adds the arrow to the right hand side.
-	cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-
-	return cell;
-
+    // If you want to add an image to your cell, here's how.
+    UIImage *image = [UIImage imageNamed:@"icon.png"];
+    cell.imageView.image = image;
+    
+    // Configure the cell to show the data.
+    NSDictionary *obj = [dataRows objectAtIndex:indexPath.row];
+    cell.textLabel.text =  [obj objectForKey:@"Name"];
+    
+    // This adds the arrow to the right hand side.
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    
+    return cell;
+    
 }
 @end
