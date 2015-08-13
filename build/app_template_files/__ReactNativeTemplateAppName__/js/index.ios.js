826ec1ccf5a3c223de41a9818c76bc0e3ee77b7a
Wolfgang Mathurin <wmathurin@salesforce.com>
2015-08-13 15:08:45 -0700

    Generated react native app now works (but doesn't use any of the salesforce react bridges yet)

--- build/app_template_files/__ReactNativeTemplateAppName__/js/index.ios.js
+++ build/app_template_files/__ReactNativeTemplateAppName__/js/index.ios.js
@@ -2,25 +2,48 @@
 
 var React = require('react-native');
 var {
+  AppRegistry,
+  StyleSheet,
   Text,
-  View
+  View,
 } = React;
 
-var styles = React.StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'red'
+var SimpleApp = React.createClass({
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
   }
 });
 
-class SimpleApp extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>This is a simple application.</Text>
-      </View>
-    )
-  }
-}
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
 
 React.AppRegistry.registerComponent('__ReactNativeTemplateAppName__', () => SimpleApp);
