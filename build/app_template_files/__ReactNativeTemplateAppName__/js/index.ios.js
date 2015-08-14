1003103ee71e9520b3943ef4b65d3a777ba70201
Wolfgang Mathurin <wmathurin@salesforce.com>
2015-08-14 13:18:11 -0700

    Native reactive template app starting to look like the other ones (but not pulling data from server yet)

--- build/app_template_files/__ReactNativeTemplateAppName__/js/index.ios.js
+++ build/app_template_files/__ReactNativeTemplateAppName__/js/index.ios.js
@@ -2,48 +2,81 @@
 
 var React = require('react-native');
 var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    ListView,
+    PixelRatio,
+    NavigatorIOS
 } = React;
 
-var SimpleApp = React.createClass({
+var App = React.createClass({
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Mobile SDK Sample App',
+          component: UserList,
+        }}
+      />
     );
   }
 });
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+var UserList = React.createClass({
+    getInitialState: function() {
+      var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+      return {
+          dataSource: ds.cloneWithRows(['User 1', 'User 2', 'User 3']),
+      };
+    },
+    
+    render: function() {
+        return (
+            <ListView
+              dataSource={this.state.dataSource}
+              renderRow={this.renderRow} />
+      );
+    },
+
+    renderRow: function(rowData: Object) {
+        return (
+                <View>
+                    <View style={styles.row}>
+                      <Text numberOfLines={1}>
+                       {rowData}
+                      </Text>
+                    </View>
+                    <View style={styles.cellBorder} />
+                </View>
+        );
+    }
 });
 
-React.AppRegistry.registerComponent('__ReactNativeTemplateAppName__', () => SimpleApp);
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
+    header: {
+        height: 50,
+        alignItems:'center'
+    },
+    row: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        padding: 12,
+    },
+    cellBorder: {
+        backgroundColor: 'rgba(0, 0, 0, 0.1)',
+        // Trick to get the thinest line the device can display
+        height: 1 / PixelRatio.get(),
+        marginLeft: 4,
+    },
+});
+
+React.AppRegistry.registerComponent('__ReactNativeTemplateAppName__', () => App);
