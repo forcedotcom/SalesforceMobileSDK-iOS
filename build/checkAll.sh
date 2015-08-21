ab4e3b8d6f544ad669621c86c306d32d564b6402
Wolfgang Mathurin <wmathurin@salesforce.com>
2015-08-21 14:25:26 -0700

    Added command line arguments
    build/checkAll.sh  --help
    Usage: ./build/checkAll.sh [options]
        -b, --build scheme               Build given scheme (pass all to build all the schemes)
        -t, --test scheme                Test given scheme (pass all to test all the schemes)
        -m, --max-time timeout           Maximum time that build/test is allowed to run

--- build/checkAll.sh
+++ build/checkAll.sh
@@ -1,19 +1,64 @@
 #!/usr/bin/ruby
 
 require 'timeout'
+require 'optparse'
 
+# Constants
+SCHEMES_TO_BUILD = [
+  "OCMock",
+  "OCMockLib",
+  "React",
+  "AccountEditor",
+  "ContactExplorer",
+  "FileExplorer",
+  "HybridFileExplorer",
+  "NativeSqlAggregator",
+  "NoteSync",
+  "RestAPIExplorer",
+  "SalesforceHybridSDK",
+  "SalesforceNetworkiOS",    
+  "SalesforceOAuth",
+  "SalesforceReact",
+  "SalesforceRestAPI",
+  "SalesforceSDKCommon",
+  "SalesforceSDKCore",
+  "SalesforceSecurity",
+  "SimpleSync",
+  "SmartStoreExplorer",
+  "SmartSync",
+  "SmartSyncExplorer",
+  "UserList",
+  "VFConnector"
+];
+
+
+SCHEMES_TO_TEST = [
+  "HybridPluginTestApp",
+  "OCMockLib", 
+  "SalesforceNetworkiOS",
+  "SalesforceOAuthTest",
+  "SalesforceRestAPIUnitTestApp",
+  "SalesforceSDKCommon",
+  "SalesforceSDKCoreUnitTestApp",
+  "SalesforceSecurity",
+  "SmartSync", 
+];
+  
+
+
+# Helper methods
 def printHeader(message)
   puts "---> #{message}"
 end
 
-def build(scheme) 
+def build(scheme, timeout) 
   printHeader("Building #{scheme}")
-  exec_with_timeout("xcodebuild -workspace SalesforceMobileSDK.xcworkspace -scheme #{scheme} 2>&1 | grep '^\*\*\ BUILD'", 20)
+  exec_with_timeout("xcodebuild -workspace SalesforceMobileSDK.xcworkspace -scheme #{scheme} 2>&1 | grep '^\*\*\ BUILD'", timeout)
 end
 
-def test(scheme) 
+def test(scheme, timeout) 
   printHeader("Testing #{scheme}")
-  exec_with_timeout("xcodebuild test -workspace SalesforceMobileSDK.xcworkspace -scheme #{scheme} -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO 2>&1 | grep '^\t-' | sed 's/-/Failed\ /'", 20)
+  exec_with_timeout("xcodebuild test -workspace SalesforceMobileSDK.xcworkspace -scheme #{scheme} -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO 2>&1 | grep '^\t-' | sed 's/-/Failed\ /'", timeout)
 end
 
 def exec_with_timeout(command, timeout)
@@ -22,70 +67,68 @@ def exec_with_timeout(command, timeout)
   begin
     status = Timeout::timeout(timeout) {
       Process.waitpid2(pipe.pid)
-      output = pipe.gets(nil)
+      print pipe.gets(nil)
     }
   rescue Timeout::Error
     Process.kill(15, pipe.pid)
+    puts "** Killed - took to long"
   end
   pipe.close
-  print output
 end
 
-def buildAll()
-  schemesToBuild = [
-    "OCMock",
-    "OCMockLib",
-    "React",
-    "AccountEditor",
-    "ContactExplorer",
-    "FileExplorer",
-    "HybridFileExplorer",
-    "NativeSqlAggregator",
-    "NoteSync",
-    "RestAPIExplorer",
-    "SalesforceHybridSDK",
-    "SalesforceNetworkiOS",    
-    "SalesforceOAuth",
-    "SalesforceReact",
-    "SalesforceRestAPI",
-    "SalesforceSDKCommon",
-    "SalesforceSDKCore",
-    "SalesforceSecurity",
-    "SimpleSync",
-    "SmartStoreExplorer",
-    "SmartSync",
-    "SmartSyncExplorer",
-    "UserList",
-    "VFConnector"
-  ];
-  
-  for scheme in schemesToBuild
-    build(scheme)
+# Build all schemes
+def buildAll(timeout)
+  for scheme in SCHEMES_TO_BUILD
+    build(scheme, timeout)
   end
 end
 
-def testAll()
-  schemesToTest = [
-    "HybridPluginTestApp",
-    "OCMockLib", 
-    "SalesforceNetworkiOS",
-    "SalesforceOAuthTest",
-    "SalesforceRestAPIUnitTestApp",
-    "SalesforceSDKCommon",
-    "SalesforceSDKCoreUnitTestApp",
-    "SalesforceSecurity",
-    "SmartSync", 
-  ];
-  
-  for scheme in schemesToTest
-    test(scheme)
+# Test all test schemes
+def testAll(timeout)
+  for scheme in SCHEMES_TO_TEST
+    test(scheme, timeout)
   end
 end
 
-def main()
-  buildAll()
-  testAll()
+# Main method
+def main(args)
+  schemeToBuild = ""
+  schemeToTest = ""
+  timeout = 30
+  
+  parser = OptionParser.new do |opts|
+    opts.banner = "Usage: ./build/checkAll.sh [options]"
+    opts.on("-b", "--build scheme", "Build given scheme (pass all to build all the schemes)") do |scheme|
+      schemeToBuild = scheme
+    end
+
+    opts.on("-t", "--test scheme", "Test given scheme (pass all to test all the schemes)") do |scheme|
+      schemeToTest = scheme
+    end
+
+    opts.on("-m", "--max-time timeout", Integer, "Maximum time that build/test is allowed to run") do |maxtime|
+      timeout = maxtime
+    end
+  end
+
+  parser.parse(args)
+
+  if (schemeToBuild != "") 
+    if (schemeToBuild == "all")
+      buildAll(timeout)
+    else
+      build(schemeToBuild, timeout)
+    end
+  end
+    
+  if (schemeToTest != "") 
+    if (schemeToTest == "all")
+      testAll(timeout)
+    else
+      test(schemeToTest, timeout)
+    end
+  end
 end
 
 #
-main()
+main(ARGV)
