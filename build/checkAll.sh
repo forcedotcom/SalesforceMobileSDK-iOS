e1908f234b0c3e33302289a83489f0bab79844a0
Wolfgang Mathurin <wmathurin@salesforce.com>
2015-08-21 14:24:16 -0700

    Beefing up checkAll.sh script
    * It builds all the schemes in the MobileSDK workspace
    * It run all the tests
    * xcodebuild is run in a separate thread which is killed if it takes too long, so nothing should stop checkAll.sh from going to the end
    * output for builds is a one liner: build succeeded or build failed, you will need to build from Xcode to get more details
    * output for tests is the list of failed tests and nothing else

--- build/checkAll.sh
+++ build/checkAll.sh
@@ -1,16 +1,91 @@
 #!/usr/bin/ruby
 
+require 'timeout'
 
-schemesToBuild = ["SalesforceSDKCommon", "SalesforceSecurity", "SalesforceOAuth", "SalesforceSDKCore", "SalesforceNetworkiOS", "SalesforceRestAPI", "SmartSync", "SalesforceHybridSDK"];
-schemesToTest = [ "SalesforceSDKCommon", "SalesforceSecurity", "SalesforceOAuthTest", "SalesforceSDKCoreUnitTestApp", "SalesforceNetworkiOS", "SalesforceRestAPIUnitTestApp", "SmartSync", "SalesforceHybridSDK", "OCMockLib", "HybridPluginTestApp", "SalesforceNetworkiOSTests"];
-
-for scheme in schemesToBuild
-  puts "\n***********************************\nBuilding #{scheme}\n***********************************"
-  system "xcodebuild -workspace SalesforceMobileSDK.xcworkspace -scheme #{scheme} > /dev/null" 
+def printHeader(message)
+  puts "---> #{message}"
 end
 
-for scheme in schemesToTest
-  puts "\n***********************************\nTesting #{scheme}\n***********************************"
-  system "xcodebuild test -workspace SalesforceMobileSDK.xcworkspace -scheme #{scheme} -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO | grep Executed"
+def build(scheme) 
+  printHeader("Building #{scheme}")
+  exec_with_timeout("xcodebuild -workspace SalesforceMobileSDK.xcworkspace -scheme #{scheme} 2>&1 | grep '^\*\*\ BUILD'", 20)
 end
 
+def test(scheme) 
+  printHeader("Testing #{scheme}")
+  exec_with_timeout("xcodebuild test -workspace SalesforceMobileSDK.xcworkspace -scheme #{scheme} -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO 2>&1 | grep '^\t-' | sed 's/-/Failed\ /'", 20)
+end
+
+def exec_with_timeout(command, timeout)
+  pipe = IO.popen(command, 'r')
+  output = ""
+  begin
+    status = Timeout::timeout(timeout) {
+      Process.waitpid2(pipe.pid)
+      output = pipe.gets(nil)
+    }
+  rescue Timeout::Error
+    Process.kill(15, pipe.pid)
+  end
+  pipe.close
+  print output
+end
+
+def buildAll()
+  schemesToBuild = [
+    "OCMock",
+    "OCMockLib",
+    "React",
+    "AccountEditor",
+    "ContactExplorer",
+    "FileExplorer",
+    "HybridFileExplorer",
+    "NativeSqlAggregator",
+    "NoteSync",
+    "RestAPIExplorer",
+    "SalesforceHybridSDK",
+    "SalesforceNetworkiOS",    
+    "SalesforceOAuth",
+    "SalesforceReact",
+    "SalesforceRestAPI",
+    "SalesforceSDKCommon",
+    "SalesforceSDKCore",
+    "SalesforceSecurity",
+    "SimpleSync",
+    "SmartStoreExplorer",
+    "SmartSync",
+    "SmartSyncExplorer",
+    "UserList",
+    "VFConnector"
+  ];
+  
+  for scheme in schemesToBuild
+    build(scheme)
+  end
+end
+
+def testAll()
+  schemesToTest = [
+    "HybridPluginTestApp",
+    "OCMockLib", 
+    "SalesforceNetworkiOS",
+    "SalesforceOAuthTest",
+    "SalesforceRestAPIUnitTestApp",
+    "SalesforceSDKCommon",
+    "SalesforceSDKCoreUnitTestApp",
+    "SalesforceSecurity",
+    "SmartSync", 
+  ];
+  
+  for scheme in schemesToTest
+    test(scheme)
+  end
+end
+
+def main()
+  buildAll()
+  testAll()
+end
+
+#
+main()
