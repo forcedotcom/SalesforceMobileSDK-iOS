b241b5c4a9ac377317063f631b79c0e051e0986c
Bharath Hariharan <bhariharan@salesforce.com>
2016-02-01 14:23:23 -0800

    Add Id and LastModifiedDate if they're missing

--- native/SampleApps/SmartSyncExplorer/SmartSyncExplorer/Classes/Data/SObjectDataManager.m
+++ native/SampleApps/SmartSyncExplorer/SmartSyncExplorer/Classes/Data/SObjectDataManager.m
@@ -85,7 +85,7 @@ static char* const kSearchFilterQueueName = "com.salesforce.smartSyncExplorer.se
     
     if (self.syncDownId == 0) {
         // first time
-        NSString *soqlQuery = [NSString stringWithFormat:@"SELECT %@, LastModifiedDate FROM %@ LIMIT %lu", [self.dataSpec.fieldNames componentsJoinedByString:@","], self.dataSpec.objectType, (unsigned long)kSyncLimit];
+        NSString *soqlQuery = [NSString stringWithFormat:@"SELECT %@ FROM %@ LIMIT %lu", [self.dataSpec.fieldNames componentsJoinedByString:@","], self.dataSpec.objectType, (unsigned long)kSyncLimit];
         SFSyncOptions *syncOptions = [SFSyncOptions newSyncOptionsForSyncDown:SFSyncStateMergeModeLeaveIfChanged];
         SFSyncDownTarget *syncTarget = [SFSoqlSyncDownTarget newSyncTarget:soqlQuery];
         [self.syncMgr syncDownWithTarget:syncTarget options:syncOptions soupName:self.dataSpec.soupName updateBlock:updateBlock];
