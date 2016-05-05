e4f552df2870a33c0bba8b970cb92569c8aebede
Arnold Goldberg <agoldberg@salesforce.com>
2016-05-05 16:02:51 -0500

    Removed comment mentioning internal class.

--- libs/SalesforceSDKCore/SalesforceSDKCore/Classes/Common/SFApplication.m
+++ libs/SalesforceSDKCore/SalesforceSDKCore/Classes/Common/SFApplication.m
@@ -29,7 +29,7 @@
 @property (atomic, readwrite, strong) NSDate *lastEventDate;
 
 /**
- * Boolean which defaults to NO, and is only set to YES in the viewDidAppear of ChatterPasscodeViewController and is set to NO in the dealloc of the VC.
+ * Boolean which defaults to NO, and is only set to YES in the viewDidAppear and is set to NO in the dealloc of the VC.
  * The boolean is used to ignore the keypressed & touch gestures in the UIApplication's sendEvents & keyPressed methods.  Otherwise, the lastEventDate
  * variable was being updated and if the timing was right on the Notification/Control Center displaying the PIN Code View could be circumvented.
  */
