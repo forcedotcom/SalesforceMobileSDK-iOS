353dbd060eadf8521a522926970c6ffb40267daf
mhu <mhu@salesforce.com>
2016-05-11 10:34:27 -0700

    add a reporter to filter unneeded logs

---
+++ reporter.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import fileinput
+import json
+import sys
+
+for line in fileinput.input():
+    if "write-file" in line:
+            sys.stdout.write('skipped')
+        else:
+            sys.stdout.write(line)
+
+sys.stdout.write('\n')
