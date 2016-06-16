199bdc28a923ba793c0b65e0a9e3bda2480c9bc7
Bharath Hariharan <bhariharan@salesforce.com>
2016-06-16 11:33:04 -0700

    Adding Transform classes

--- libs/SalesforceAnalytics/SalesforceAnalytics/Classes/Transform/AILTNTransform.h
+++ libs/SalesforceAnalytics/SalesforceAnalytics/Classes/Transform/AILTNTransform.h
@@ -1,9 +1,9 @@
 /*
- SalesforceAnalytics.h
+ AILTNTransform.h
  SalesforceAnalytics
-
- Created by Bharath Hariharan on Thu Jun 16 11:28:19 PDT 2016.
-
+ 
+ Created by Bharath Hariharan on 6/16/16.
+ 
  Copyright (c) 2016, salesforce.com, inc. All rights reserved.
  
  Redistribution and use of this software in source and binary forms, with or without modification,
@@ -27,11 +27,8 @@
  WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#import <SalesforceAnalytics/AILTNTransform.h>
-#import <SalesforceAnalytics/AnalyticsManager.h>
-#import <SalesforceAnalytics/DeviceAppAttributes.h>
-#import <SalesforceAnalytics/EventStoreManager.h>
-#import <SalesforceAnalytics/InstrumentationEvent.h>
-#import <SalesforceAnalytics/InstrumentationEventBuilder.h>
-#import <SalesforceAnalytics/SFSDKReachability.h>
-#import <SalesforceAnalytics/Transform.h>
+#import "Transform.h"
+
+@interface AILTNTransform : NSObject <Transform>
+
+@end
--- libs/SalesforceAnalytics/SalesforceAnalytics/Classes/Transform/AILTNTransform.m
+++ libs/SalesforceAnalytics/SalesforceAnalytics/Classes/Transform/AILTNTransform.m
@@ -1,9 +1,9 @@
 /*
- SalesforceAnalytics.h
+ AILTNTransform.m
  SalesforceAnalytics
-
- Created by Bharath Hariharan on Thu Jun 16 11:28:19 PDT 2016.
-
+ 
+ Created by Bharath Hariharan on 6/16/16.
+ 
  Copyright (c) 2016, salesforce.com, inc. All rights reserved.
  
  Redistribution and use of this software in source and binary forms, with or without modification,
@@ -27,11 +27,16 @@
  WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#import <SalesforceAnalytics/AILTNTransform.h>
-#import <SalesforceAnalytics/AnalyticsManager.h>
-#import <SalesforceAnalytics/DeviceAppAttributes.h>
-#import <SalesforceAnalytics/EventStoreManager.h>
-#import <SalesforceAnalytics/InstrumentationEvent.h>
-#import <SalesforceAnalytics/InstrumentationEventBuilder.h>
-#import <SalesforceAnalytics/SFSDKReachability.h>
-#import <SalesforceAnalytics/Transform.h>
+#import "AILTNTransform.h"
+
+@implementation AILTNTransform
+
+- (NSDictionary *) transform:(InstrumentationEvent *) event {
+
+    /*
+     * TODO: Transform to AILTN format.
+     */
+    return nil;
+}
+
+@end
