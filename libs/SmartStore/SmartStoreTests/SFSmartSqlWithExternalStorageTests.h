1db7f0bf822049020d05b60244dafee4862007d3
Wolfgang Mathurin <wmathurin@salesforce.com>
2016-07-12 16:47:19 -0600

    Avoiding code duplication between SFSmartSqlTests.m and SFSmartSqlWithExternalStorageTests.m

--- libs/SmartStore/SmartStoreTests/SFSmartSqlWithExternalStorageTests.h
+++ libs/SmartStore/SmartStoreTests/SFSmartSqlWithExternalStorageTests.h
@@ -25,7 +25,7 @@
 //  Logic unit tests contain unit test code that is designed to be linked into an independent test executable.
 //  See Also: http://developer.apple.com/iphone/library/documentation/Xcode/Conceptual/iphone_development/135-Unit_Testing_Applications/unit_testing_applications.html
 
-#import "SFSmartStoreTestCase.h"
+#import "SFSmartSqlTests.h"
 
-@interface SFSmartSqlWithExternalStorageTests : SFSmartStoreTestCase
+@interface SFSmartSqlWithExternalStorageTests : SFSmartSqlTests
 @end
