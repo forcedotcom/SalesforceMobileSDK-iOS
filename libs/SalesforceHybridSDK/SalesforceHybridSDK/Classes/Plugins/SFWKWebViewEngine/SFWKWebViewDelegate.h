3de40dd90f0636ede57305d73401cf25c35bad02
Tyson Bunch <tbunch@salesforce.com>
2016-10-28 16:11:01 -0700

    Change copyright

--- libs/SalesforceHybridSDK/SalesforceHybridSDK/Classes/Plugins/SFWKWebViewEngine/SFWKWebViewDelegate.h
+++ libs/SalesforceHybridSDK/SalesforceHybridSDK/Classes/Plugins/SFWKWebViewEngine/SFWKWebViewDelegate.h
@@ -4,7 +4,7 @@
 
  Created by Bharath Hariharan on 7/15/16.
 
- Copyright (c) 2016, salesforce.com, inc. All rights reserved.
+ Copyright (c) 2016-present, salesforce.com, inc. All rights reserved.
 
  Redistribution and use of this software in source and binary forms, with or without modification,
  are permitted provided that the following conditions are met:
--- libs/SalesforceSDKCore/SalesforceSDKCore/Classes/Util/UIColor+SFColors.h
+++ libs/SalesforceSDKCore/SalesforceSDKCore/Classes/Util/UIColor+SFColors.h
@@ -3,7 +3,7 @@
  SalesforceSDKCore
  
  Created by Kunal Chitalia on 3/28/16.
- Copyright (c) 2016, salesforce.com, inc. All rights reserved.
+ Copyright (c) 2016-present, salesforce.com, inc. All rights reserved.
  
  Redistribution and use of this software in source and binary forms, with or without modification,
  are permitted provided that the following conditions are met:
