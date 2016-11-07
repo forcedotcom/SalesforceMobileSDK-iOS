3c5d5b359487ea5e9fd85773f40fbc6c6c5a13b5
Alex Sikora <asikora@salesforce.com>
2016-11-07 18:05:05 -0500

    Build all binaries that FeedSDK needs

---
+++ build-all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ -z $WORKSPACE ]]; then
+	WORKSPACE=`(cd $(dirname $0)/; pwd)`
+fi
+
+
+./build-ios.sh -c Debug -b $WORKSPACE/derivedData -f CocoaLumberjack -g CocoaLumberjack-iOS
+./build-ios.sh -c Release -b $WORKSPACE/derivedData -f CocoaLumberjack -g CocoaLumberjack-iOS
+
+./build-ios.sh -c Debug -b $WORKSPACE/derivedData -f SalesforceAnalytics
+./build-ios.sh -c Release -b $WORKSPACE/derivedData -f SalesforceAnalytics
+
+./build-ios.sh -c Debug -b $WORKSPACE/derivedData -f SalesforceSDKCore
+./build-ios.sh -c Release -b $WORKSPACE/derivedData -f SalesforceSDKCore
