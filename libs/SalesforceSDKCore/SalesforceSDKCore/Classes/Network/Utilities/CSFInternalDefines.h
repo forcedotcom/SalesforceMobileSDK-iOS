833de2a7ee01f5d8bb5e928817d340f241f117de
Michael <mnachbaur@salesforce.com>
2017-01-24 09:52:15 -0800

    Ensure ObjC runtime is available to header for test targets

--- libs/SalesforceSDKCore/SalesforceSDKCore/Classes/Network/Utilities/CSFInternalDefines.h
+++ libs/SalesforceSDKCore/SalesforceSDKCore/Classes/Network/Utilities/CSFInternalDefines.h
@@ -22,6 +22,8 @@
  WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#import <objc/runtime.h>
+
 #import "CSFDefines.h"
 #import "CSFPrivateDefines.h"
 
