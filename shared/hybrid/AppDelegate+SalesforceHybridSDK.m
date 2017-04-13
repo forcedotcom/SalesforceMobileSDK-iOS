6af53f002a9215972247ac9bef8a6c1c97b5c493
trooper2013 <rao.r@raor-ltm1.internal.salesforce.com>
2017-04-13 14:41:05 -0700

    Adding commented code for hybrid template to handle advanced auth scenario.

--- shared/hybrid/AppDelegate+SalesforceHybridSDK.m
+++ shared/hybrid/AppDelegate+SalesforceHybridSDK.m
@@ -59,6 +59,17 @@
     // Need to use SalesforceSDKManagerWithSmartStore when using smartstore
     [SalesforceSDKManager setInstanceClass:[SalesforceSDKManagerWithSmartStore class]];
     [SalesforceSDKManager sharedManager].appConfig = appConfig;
+    [SFAuthenticationManager sharedManager].advancedAuthConfiguration = SFOAuthAdvancedAuthConfigurationRequire;
+    
+    //Uncomment the following line inorder to enable/force the use of advanced authentication flow.
+    //[SFAuthenticationManager sharedManager].advancedAuthConfiguration = SFOAuthAdvancedAuthConfigurationRequire;
+    // OR
+    // To  retrieve advanced auth configuration from the org, to determine whether to initiate advanced authentication.
+    //[SFAuthenticationManager sharedManager].advancedAuthConfiguration = SFOAuthAdvancedAuthConfigurationAllow;
+    
+    // NOTE: If advanced authentication is configured or forced,  it will launch Safari to handle authentication
+    // instead of a webview. You must implement application:openURL:options: to handle the callback.
+    
     __weak __typeof(self) weakSelf = self;
     [SalesforceSDKManager sharedManager].postLaunchAction = ^(SFSDKLaunchAction launchActionList) {
         __strong __typeof(weakSelf) strongSelf = weakSelf;
@@ -110,6 +121,17 @@
     [self sfsdk_swizzled_application:application didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
 }
 
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options{
+    
+    // Uncomment the following line, if Authentication was attempted using handle advanced OAuth flow.
+    // For Advanced Auth functionality to work, edit your apps plist files and add the URL scheme that you have
+    // chosen for your app. The scheme should be the same as used in  the oauthRedirectURI settings of your Connected
+    //  App. You should also set the  delegate(SFAuthenticationManagerDelegate) for SFAuthenticationManager to be
+    // notified  of success & failures. Inorder to be notfied of user's selected action on displayed
+    // alerts implement  authManagerDidProceedWithBrowserFlow: & authManagerDidCancelBrowserFlow:
+    // return [[SFAuthenticationManager sharedManager] handleAdvancedAuthenticationResponse:url];
+    return NO;
+}
 
 - (void)handleSdkManagerLogout
 {
