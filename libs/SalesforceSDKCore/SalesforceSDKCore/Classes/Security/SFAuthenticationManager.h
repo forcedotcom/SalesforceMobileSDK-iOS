7260d00f9362a8383ec951baf123ed7fea611f5f
trooper2013 <rao.r@raor-ltm1.internal.salesforce.com>
2017-04-20 22:03:11 -0700

    Cookie handling for WKWebView

--- libs/SalesforceSDKCore/SalesforceSDKCore/Classes/Security/SFAuthenticationManager.h
+++ libs/SalesforceSDKCore/SalesforceSDKCore/Classes/Security/SFAuthenticationManager.h
@@ -470,16 +470,28 @@ extern  NSString * const kOAuthRedirectUriKey;
 + (BOOL)errorIsInvalidAuthCredentials:(NSError *)error;
 
 /**
- Remove any cookies with the given names from the given domains.
- @param cookieNames The names of the cookies to remove.
+ Remove all cookies for the given domains. Fire and Forget.
  @param domainNames The names of the domains where the cookies are set.
  */
-+ (void)removeCookies:(NSArray<NSString*> *)cookieNames fromDomains:(NSArray<NSString*> *)domainNames;
++ (void)removeCookiesFromDomains:(NSArray<NSString*> *)domainNames;
+
+/**
+ Remove all cookies for the given domains. Call completion block when done.
+ @param domainNames The names of the domains where the cookies are set.
+ @param completionBlock Block invoked when cookies are removed
+ */
++ (void)removeCookiesFromDomains:(NSArray<NSString*> *)domainNames withCompletion:(nullable void(^)())completionBlock;
+
+/**
+ Remove all cookies from the cookie store. Fire and forget.
+ */
++ (void)removeAllCookies;
 
 /**
  Remove all cookies from the cookie store.
+ @param completionBlock Block invoked when cookies are removed
  */
-+ (void)removeAllCookies;
++ (void)removeAllCookiesWithCompletion:(nullable void(^)())completionBlock;
 
 /**
  Adds the access (session) token cookie to the web view, for authentication.
