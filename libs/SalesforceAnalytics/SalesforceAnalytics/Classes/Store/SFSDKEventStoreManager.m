4b79b1da80f7afc45b0275d9f59e33e3fe206d50
Bharath Hariharan <bhariharan@salesforce.com>
2017-04-28 10:12:56 -0700

    Avoid throwing an exception for missing mandatory fields while building event

--- libs/SalesforceAnalytics/SalesforceAnalytics/Classes/Store/SFSDKEventStoreManager.m
+++ libs/SalesforceAnalytics/SalesforceAnalytics/Classes/Store/SFSDKEventStoreManager.m
@@ -69,6 +69,9 @@
 }
 
 - (void) storeEvent:(SFSDKInstrumentationEvent *) event {
+    if (!event) {
+        return;
+    }
 
     // Copies event, to isolate data for I/O.
     SFSDKInstrumentationEvent *eventCopy = [event copy];
