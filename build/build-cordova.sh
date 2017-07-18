4026c523fbc75ed46f2c2584c2cc8a894d3d1d6f
Bharath Hariharan <bhariharan@salesforce.com>
2017-07-17 17:14:42 -0700

    Minor tweaks to build script

--- build/build-cordova.sh
+++ build/build-cordova.sh
@@ -6,20 +6,20 @@ fi
 
 SCRIPT_DIR=`(cd $(dirname $0)/; pwd)`
 
-$SCRIPT_DIR/build-ios.sh -c Debug -b $WORKSPACE/derivedData -f CocoaLumberjack -g CocoaLumberjack-iOS
-$SCRIPT_DIR/build-ios.sh -c Release -b $WORKSPACE/derivedData -f CocoaLumberjack -g CocoaLumberjack-iOS
+$SCRIPT_DIR/build-ios.sh -c Debug -f CocoaLumberjack -g CocoaLumberjack-iOS
+$SCRIPT_DIR/build-ios.sh -c Release -f CocoaLumberjack -g CocoaLumberjack-iOS
 
-$SCRIPT_DIR/build-ios.sh -c Debug -b $WORKSPACE/derivedData -f SalesforceAnalytics
-$SCRIPT_DIR/build-ios.sh -c Release -b $WORKSPACE/derivedData -f SalesforceAnalytics
+$SCRIPT_DIR/build-ios.sh -c Debug -f SalesforceAnalytics
+$SCRIPT_DIR/build-ios.sh -c Release -f SalesforceAnalytics
 
-$SCRIPT_DIR/build-ios.sh -c Debug -b $WORKSPACE/derivedData -f SalesforceSDKCore
-$SCRIPT_DIR/build-ios.sh -c Release -b $WORKSPACE/derivedData -f SalesforceSDKCore
+$SCRIPT_DIR/build-ios.sh -c Debug -f SalesforceSDKCore
+$SCRIPT_DIR/build-ios.sh -c Release -f SalesforceSDKCore
 
-$SCRIPT_DIR/build-ios.sh -c Debug -b $WORKSPACE/derivedData -f SmartStore
-$SCRIPT_DIR/build-ios.sh -c Release -b $WORKSPACE/derivedData -f SmartStore
+$SCRIPT_DIR/build-ios.sh -c Debug -f SmartStore
+$SCRIPT_DIR/build-ios.sh -c Release -f SmartStore
 
-$SCRIPT_DIR/build-ios.sh -c Debug -b $WORKSPACE/derivedData -f SmartSync
-$SCRIPT_DIR/build-ios.sh -c Release -b $WORKSPACE/derivedData -f SmartSync
+$SCRIPT_DIR/build-ios.sh -c Debug -f SmartSync
+$SCRIPT_DIR/build-ios.sh -c Release -f SmartSync
 
-$SCRIPT_DIR/build-ios.sh -c Debug -b $WORKSPACE/derivedData -f SalesforceHybridSDK -g SalesforceHybridSDKCordovaPlugin
-$SCRIPT_DIR/build-ios.sh -c Release -b $WORKSPACE/derivedData -f SalesforceHybridSDK -g SalesforceHybridSDKCordovaPlugin
+$SCRIPT_DIR/build-ios.sh -c Debug -f SalesforceHybridSDK -g SalesforceHybridSDKCordovaPlugin
+$SCRIPT_DIR/build-ios.sh -c Release -f SalesforceHybridSDK -g SalesforceHybridSDKCordovaPlugin
