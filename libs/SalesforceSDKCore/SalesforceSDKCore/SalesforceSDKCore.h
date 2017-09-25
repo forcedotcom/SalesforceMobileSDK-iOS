035dab6641f8c810e2f86eb6b30db49a2c737144
rao-r <rao.r@raor-ltm1.internal.salesforce.com>
2017-09-24 21:44:04 -0700

    Moving classes to project scope

--- libs/SalesforceSDKCore/SalesforceSDKCore/SalesforceSDKCore.h
+++ libs/SalesforceSDKCore/SalesforceSDKCore/SalesforceSDKCore.h
@@ -2,7 +2,7 @@
  SalesforceSDKCore.h
  SalesforceSDKCore
 
- Created by Raj Rao on Sat Sep 23 19:35:44 PDT 2017.
+ Created by Raj Rao on Sun Sep 24 21:41:27 PDT 2017.
 
  Copyright (c) 2017-present, salesforce.com, inc. All rights reserved.
  
@@ -108,7 +108,6 @@
 #import <SalesforceSDKCore/SFSDKLoginHostDelegate.h>
 #import <SalesforceSDKCore/SFSDKLoginHostListViewController.h>
 #import <SalesforceSDKCore/SFSDKLoginHostStorage.h>
-#import <SalesforceSDKCore/SFSDKOAuthClientContext.h>
 #import <SalesforceSDKCore/SFSDKResourceUtils.h>
 #import <SalesforceSDKCore/SFSDKSafeMutableDictionary.h>
 #import <SalesforceSDKCore/SFSDKSalesforceAnalyticsManager.h>
