fc19221c185dda295f2f2b517ef936ade57fc041
trooper2013 <>
2017-10-06 08:59:02 -0700

    Removed unnecessary code blocks

--- native/SampleApps/SmartSyncExplorer/SmartSyncExplorer/Classes/IDPLoginViewController.m
+++ native/SampleApps/SmartSyncExplorer/SmartSyncExplorer/Classes/IDPLoginViewController.m
@@ -47,19 +47,7 @@
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    
-    // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 - (IBAction)loginIDPAction:(id)sender {
     if ([self.loginSelectionDelegate respondsToSelector:@selector(loginUsingIDP)]) {
