ed45b3aa34eed06366525fec3430f84ca5c3cea2
trooper2013 <>
2017-11-28 13:13:54 -0800

    Initial Swift SDK setup

---
+++ libs/SalesforceSwiftSDK/SalesforceSwiftSDKTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SalesforceSwiftSDKTestApp
+//
+//  Created by Raj Rao on 11/28/17.
+//  Copyright © 2017 Raj Rao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
