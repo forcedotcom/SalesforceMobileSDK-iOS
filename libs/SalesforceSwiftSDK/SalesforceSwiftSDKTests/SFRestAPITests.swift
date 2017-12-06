7072d3df12cfd49d82342b52d26b051b1cc7ff2a
Bharath Hariharan <bhariharan@salesforce.com>
2017-12-05 21:22:26 -0800

    Adding scripts for pod specs and umbrella header

--- libs/SalesforceSwiftSDK/SalesforceSwiftSDKTests/SFRestAPITests.swift
+++ libs/SalesforceSwiftSDK/SalesforceSwiftSDKTests/SFRestAPITests.swift
@@ -22,11 +22,11 @@
  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
  WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 import Foundation
 import XCTest
 import SalesforceSDKCore
 import PromiseKit
+
 @testable import SalesforceSwiftSDK
 
 class SFRestAPITests: XCTestCase {
