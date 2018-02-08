6d5d2416742a9b0e40878fc0c00e35bdc0621d03
Wolfgang Mathurin <wmathurin@salesforce.com>
2018-02-08 09:26:20 -0800

    Misc fixes

--- libs/SalesforceReact/SalesforceReactTests/SFNetReactBridge.m
+++ libs/SalesforceReact/SalesforceReactTests/SFNetReactBridge.m
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2016-present, salesforce.com, inc. All rights reserved.
+ Copyright (c) 2018-present, salesforce.com, inc. All rights reserved.
  
  Redistribution and use of this software in source and binary forms, with or without modification,
  are permitted provided that the following conditions are met:
