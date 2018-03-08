839bbfacc287bf4d7179169577179030a04108de
Bharath Hariharan <bhariharan@salesforce.com>
2018-03-07 21:10:50 -0800

    Replacing SFNetwork with SFRestAPI for network requests

--- libs/SalesforceSDKCore/SalesforceSDKCore/Classes/PushNotification/SFPushNotificationManager.h
+++ libs/SalesforceSDKCore/SalesforceSDKCore/Classes/PushNotification/SFPushNotificationManager.h
@@ -82,7 +82,7 @@ NS_ASSUME_NONNULL_BEGIN
  * @param completionBlock Completion block.
  * @return YES for successful unregistration call being made.
  */
-- (BOOL)unregisterSalesforceNotificationsWithCompletionBlock:(SFUserAccount*)user completionBlock:(void (^)(void))completionBlock;
+- (BOOL)unregisterSalesforceNotificationsWithCompletionBlock:(SFUserAccount*)user completionBlock:(nullable void (^)(void))completionBlock;
 
 @end
 
