a8c0a06d4ad79b65bd3a2b11cc36e95737dcd0af
Raj Rao <rao.r@salesforce.com>
2019-02-05 08:15:46 -0800

    Fix License header

--- libs/SalesforceSDKCore/SalesforceSDKCore/Classes/Util/SFSDKViewUtils.m
+++ libs/SalesforceSDKCore/SalesforceSDKCore/Classes/Util/SFSDKViewUtils.m
@@ -3,7 +3,7 @@
  SalesforceSDKCore
  
  Created by Raj Rao on 2/5/19.
- Copyright (c) 2018-present, salesforce.com, inc. All rights reserved.
+ Copyright (c) 2019-present, salesforce.com, inc. All rights reserved.
  
  Redistribution and use of this software in source and binary forms, with or without modification,
  are permitted provided that the following conditions are met:
