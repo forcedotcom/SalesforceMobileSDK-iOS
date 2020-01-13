483df171a0e943f63e0e900c774493beca007ba9
maliroteh <52436329+maliroteh@users.noreply.github.com>
2020-01-13 12:07:29 -0800

    Add missing header file

--- libs/MobileSync/MobileSync/Classes/Util/SFMobileSyncNetworkUtils.m
+++ libs/MobileSync/MobileSync/Classes/Util/SFMobileSyncNetworkUtils.m
@@ -24,6 +24,7 @@
 
 #import "SFMobileSyncNetworkUtils.h"
 #import <SalesforceSDKCore/SFRestRequest.h>
+#import "SFUserAccountManager.h"
 
 // For user agent
 NSString * const kUserAgent = @"User-Agent";
