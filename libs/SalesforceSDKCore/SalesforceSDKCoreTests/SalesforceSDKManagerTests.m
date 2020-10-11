c7f0018bb4cfde894b0383f65a7210462f8d23e0
Brianna Birman <brianna.birman@salesforce.com>
2020-10-10 17:19:39 -0700

    Remove SalesforceSDKManager deprecations

--- libs/SalesforceSDKCore/SalesforceSDKCoreTests/SalesforceSDKManagerTests.m
+++ libs/SalesforceSDKCore/SalesforceSDKCoreTests/SalesforceSDKManagerTests.m
@@ -32,7 +32,6 @@
 #import "SFUserAccount+Internal.h"
 #import "SFOAuthCredentials+Internal.h"
 
-static NSTimeInterval const kTimeDelaySecsBetweenLaunchSteps = 0.5;
 static NSString* const kTestAppName = @"OverridenAppName";
 
 @interface SalesforceSDKManagerTests : XCTestCase
@@ -42,18 +41,9 @@ static NSString* const kTestAppName = @"OverridenAppName";
     NSSet *_origAuthScopes;
     BOOL _origAuthenticateAtLaunch;
     BOOL _origHasVerifiedPasscodeAtStartup;
-    SFSDKPostLaunchCallbackBlock _origPostLaunchAction;
-    SFSDKLaunchErrorCallbackBlock _origLaunchErrorAction;
-    SFSDKLogoutCallbackBlock _origPostLogoutAction;
-    SFSDKSwitchUserCallbackBlock _origSwitchUserAction;
-    SFSDKAppForegroundCallbackBlock _origPostAppForegroundAction;
     SFUserAccount *_origCurrentUser;
     id<SalesforceSDKManagerFlow> _origSdkManagerFlow;
     SFTestSDKManagerFlow *_currentSdkManagerFlow;
-    BOOL _postLaunchBlockCalled;
-    SFSDKLaunchAction _postLaunchActions;
-    BOOL _launchErrorBlockCalled;
-    NSError *_launchError;
     NSString *_origAppName;
     WKProcessPool *_origProcessPool;
     NSString *_origBrandLoginPath;
@@ -66,8 +56,6 @@ static NSString* const kTestAppName = @"OverridenAppName";
 - (void)setUp
 {
     [super setUp];
-    XCTAssertFalse([SalesforceSDKManager sharedManager].isLaunching, @"SalesforceSDKManager should not be launching at the beginning of the test.");
-
     // Since other tests may have a more "permanent" idea of (essentially global) app identity and
     // launch state, only clear values for the length of the test, then restore them.
     [self setupSdkManagerState];
@@ -75,91 +63,35 @@ static NSString* const kTestAppName = @"OverridenAppName";
 
 - (void)tearDown
 {
-
     // Restore the original SDK Manager state (see setUp).
     [self restoreOrigSdkManagerState];
     [super tearDown];
 }
 
-- (void)testValidateInput
-{
-
-    // Set nothing, validate errors.
-    [self createStandardLaunchErrorBlock];
-    [self launchAndVerify:YES failMessage:@"Failed to start launch."];
-    [self verifyLaunchErrorState:YES];
-    XCTAssertEqual([_launchError domain], kSalesforceSDKManagerErrorDomain, @"Wrong error domain.");
-    XCTAssertEqual([_launchError code], kSalesforceSDKManagerErrorInvalidLaunchParameters, @"Wrong error code.");
-    XCTAssertEqual([[_launchError userInfo][kSalesforceSDKManagerErrorDetailsKey] count], 1UL, @"There should have been one fatal validation error.");
-    
-    // Set Connected App ID
-    [SalesforceSDKManager sharedManager].appConfig.remoteAccessConsumerKey = @"test_connected_app_id";
-    [self launchAndVerify:YES failMessage:@"Failed to start launch."];
-    [self verifyLaunchErrorState:YES];
-    XCTAssertEqual([_launchError domain], kSalesforceSDKManagerErrorDomain, @"Wrong error domain.");
-    XCTAssertEqual([_launchError code], kSalesforceSDKManagerErrorInvalidLaunchParameters, @"Wrong error code.");
-    XCTAssertEqual([[_launchError userInfo][kSalesforceSDKManagerErrorDetailsKey] count], 1UL, @"There should have been one fatal validation error.");
-    
-    // Set Callback URI
-    [SalesforceSDKManager sharedManager].appConfig.oauthRedirectURI = @"test_connected_app_callback_uri";
-    [self launchAndVerify:YES failMessage:@"Failed to start launch."];
-    [self verifyLaunchErrorState:YES];
-    XCTAssertEqual([_launchError domain], kSalesforceSDKManagerErrorDomain, @"Wrong error domain.");
-    XCTAssertEqual([_launchError code], kSalesforceSDKManagerErrorInvalidLaunchParameters, @"Wrong error code.");
-    XCTAssertEqual([[_launchError userInfo][kSalesforceSDKManagerErrorDetailsKey] count], 1UL, @"There should have been one fatal validation error.");
-    
-    // Set auth scopes
-    [SalesforceSDKManager sharedManager].appConfig.oauthScopes = [NSSet setWithArray:@[ @"web", @"api" ]];
-    [self launchAndVerify:YES failMessage:@"Failed to start launch."];
-    [self verifyLaunchErrorState:NO];
-}
-
-- (void)testOneLaunchAtATime
-{
-    [self createStandardPostLaunchBlock];
-    [self createTestAppIdentity];
-    [self launchAndVerify:YES failMessage:@"Initial launch attempt should have been successful."];
-    [self launchAndVerify:NO failMessage:@"Second concurrent launch should not be allowed."];
-    [self verifyPostLaunchState];
-}
-
-- (void)testOverrideAiltnAppNameBeforeSDKManagerLaunch
+- (void)testOverrideAiltnAppNameBeforeSDKManagerInit
 {
     [SalesforceSDKManager setAiltnAppName:kTestAppName];
-    [self createStandardPostLaunchBlock];
     [self createTestAppIdentity];
-    [self launchAndVerify:YES failMessage:@"Launch attempt should have been successful."];
-    [self verifyPostLaunchState];
     [self compareAiltnAppNames:kTestAppName];
 }
 
 - (void)testOverrideAiltnAppNameAfterSDKManagerInit
 {
-    [self createStandardPostLaunchBlock];
     [self createTestAppIdentity];
-    [self launchAndVerify:YES failMessage:@"Launch attempt should have been successful."];
-    [self verifyPostLaunchState];
     [SalesforceSDKManager setAiltnAppName:kTestAppName];
     [self compareAiltnAppNames:kTestAppName];
 }
 
 - (void)testDefaultAiltnAppName
 {
-    [self createStandardPostLaunchBlock];
     [self createTestAppIdentity];
-    [self launchAndVerify:YES failMessage:@"Launch attempt should have been successful."];
-    [self verifyPostLaunchState];
     NSString *appName = [[NSBundle mainBundle] infoDictionary][(NSString *) kCFBundleNameKey];
     [self compareAiltnAppNames:appName];
 }
 
 - (void)testOverrideInvalidAiltnAppName
 {
-    [[SFUserAccountManager sharedInstance] setCurrentUserInternal:[self createUserAccount]];
-    [self createStandardPostLaunchBlock];
     [self createTestAppIdentity];
-    [self launchAndVerify:YES failMessage:@"Launch attempt should have been successful."];
-    [self verifyPostLaunchState];
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wnonnull"
     [SalesforceSDKManager setAiltnAppName:nil];
@@ -168,106 +100,30 @@ static NSString* const kTestAppName = @"OverridenAppName";
     [self compareAiltnAppNames:appName];
 }
 
-- (void)testOverrideAppNameBeforeSDKManagerLaunch
+- (void)testOverrideAppNameBeforeSDKManagerInit
 {
     [SalesforceSDKManager setAppName:kTestAppName];
-    [self createStandardPostLaunchBlock];
     [self createTestAppIdentity];
-    [self launchAndVerify:YES failMessage:@"Launch attempt should have been successful."];
-    [self verifyPostLaunchState];
     [self compareAppNames:kTestAppName];
 }
 
 - (void)testOverrideAppNameAfterSDKManagerInit
 {
-    [self createStandardPostLaunchBlock];
     [self createTestAppIdentity];
-    [self launchAndVerify:YES failMessage:@"Launch attempt should have been successful."];
-    [self verifyPostLaunchState];
     [SalesforceSDKManager setAppName:kTestAppName];
     [self compareAppNames:kTestAppName];
 }
 
 - (void)testDefaultAppName
 {
-    [self createStandardPostLaunchBlock];
     [self createTestAppIdentity];
-    [self launchAndVerify:YES failMessage:@"Launch attempt should have been successful."];
-    [self verifyPostLaunchState];
     NSString *appName = [[NSBundle mainBundle] infoDictionary][(NSString *) kCFBundleNameKey];
     [self compareAppNames:appName];
-}
-
-- (void)testPasscodeVerificationAtLaunch
-{
-    [self createStandardPostLaunchBlock];
-    [self createTestAppIdentity];
-    [self launchAndVerify:YES failMessage:@"Launch attempt should have been successful."];
-    [self verifyPostLaunchState];
-    BOOL passcodeVerifiedOnInitialLaunch = ((_postLaunchActions & SFSDKLaunchActionPasscodeVerified) == SFSDKLaunchActionPasscodeVerified);
-    XCTAssertTrue(passcodeVerifiedOnInitialLaunch, @"Passcode should have been verified on initial launch. Actual state: %@", [SalesforceSDKManager launchActionsStringRepresentation:_postLaunchActions]);
-    [self launchAndVerify:YES failMessage:@"Launch attempt should have been successful."];
-    [self verifyPostLaunchState];
-    passcodeVerifiedOnInitialLaunch = ((_postLaunchActions & SFSDKLaunchActionPasscodeVerified) == SFSDKLaunchActionPasscodeVerified);
-    XCTAssertFalse(passcodeVerifiedOnInitialLaunch, @"Passcode should NOT have been verified on subsequent launches.");
-}
-
-- (void)testAuthAtLaunch
-{
-    XCTAssertNil([SFUserAccountManager sharedInstance].currentUser, @"Current user should be nil.");
-    [self createStandardPostLaunchBlock];
-    [self createTestAppIdentity];
-    [[SFUserAccountManager sharedInstance] setCurrentUserInternal:[self createUserAccount]];
-    [self launchAndVerify:YES failMessage:@"Launch attempt should have been successful."];
-    [self verifyPostLaunchState];
-    BOOL userAuthenticatedAtLaunch = ((_postLaunchActions & SFSDKLaunchActionAuthenticated) == SFSDKLaunchActionAuthenticated);
-    BOOL userAlreadyAuthenticated = ((_postLaunchActions & SFSDKLaunchActionAlreadyAuthenticated) == SFSDKLaunchActionAlreadyAuthenticated);
-    BOOL authBypassed = ((_postLaunchActions & SFSDKLaunchActionAuthBypassed) == SFSDKLaunchActionAuthBypassed);
-    XCTAssertTrue(userAuthenticatedAtLaunch, @"User without credentials should have been authenticated at launch.");
-    XCTAssertFalse(userAlreadyAuthenticated, @"User without credentials should not have generated an already-authenticated status.");
-    XCTAssertFalse(authBypassed, @"User without credentials should not have generated an auth-bypassed status.");
-    [SFUserAccountManager sharedInstance].currentUser.credentials.accessToken = @"test_access_token";
-    [self launchAndVerify:YES failMessage:@"Launch attempt should have been successful."];
-    [self verifyPostLaunchState];
-    userAuthenticatedAtLaunch = ((_postLaunchActions & SFSDKLaunchActionAuthenticated) == SFSDKLaunchActionAuthenticated);
-    userAlreadyAuthenticated = ((_postLaunchActions & SFSDKLaunchActionAlreadyAuthenticated) == SFSDKLaunchActionAlreadyAuthenticated);
-    authBypassed = ((_postLaunchActions & SFSDKLaunchActionAuthBypassed) == SFSDKLaunchActionAuthBypassed);
-    XCTAssertFalse(userAuthenticatedAtLaunch, @"User with credentials should not have been authenticated at launch.");
-    XCTAssertTrue(userAlreadyAuthenticated, @"User with credentials should have generated an already-authenticated status.");
-    XCTAssertFalse(authBypassed, @"User with credentials should not have generated an auth-bypassed status.");
-    NSError *error = nil;
-    [[SFUserAccountManager sharedInstance] deleteAccountForUser:[SFUserAccountManager sharedInstance].currentUser error:&error];
-    XCTAssertNil(error, @"SalesforceSDKManagerTests for testAuthAtLaunch could not delete created user");
-}
-
-- (void)testAuthBypass
-{
-    XCTAssertNil([SFUserAccountManager sharedInstance].currentUser, @"Current user should be nil.");
-    [SalesforceSDKManager sharedManager].appConfig.shouldAuthenticate = NO;
-    [self createStandardPostLaunchBlock];
-    [self createTestAppIdentity];
-    [[SFUserAccountManager sharedInstance] setCurrentUserInternal:[self createUserAccount]];
-    [self launchAndVerify:YES failMessage:@"Launch attempt should have been successful."];
-    [self verifyPostLaunchState];
-    BOOL userAuthenticatedAtLaunch = ((_postLaunchActions & SFSDKLaunchActionAuthenticated) == SFSDKLaunchActionAuthenticated);
-    BOOL userAlreadyAuthenticated = ((_postLaunchActions & SFSDKLaunchActionAlreadyAuthenticated) == SFSDKLaunchActionAlreadyAuthenticated);
-    BOOL authBypassed = ((_postLaunchActions & SFSDKLaunchActionAuthBypassed) == SFSDKLaunchActionAuthBypassed);
-    XCTAssertFalse(userAuthenticatedAtLaunch, @"User should not generate an authenticated status at launch when bypass is configured.");
-    XCTAssertFalse(userAlreadyAuthenticated, @"User should not generate an already-authenticated status when bypass is configured.");
-    XCTAssertTrue(authBypassed, @"Launch should have generated an auth-bypassed status.");
-    [SFUserAccountManager sharedInstance].currentUser.credentials.accessToken = @"test_access_token";
-    userAuthenticatedAtLaunch = ((_postLaunchActions & SFSDKLaunchActionAuthenticated) == SFSDKLaunchActionAuthenticated);
-    userAlreadyAuthenticated = ((_postLaunchActions & SFSDKLaunchActionAlreadyAuthenticated) == SFSDKLaunchActionAlreadyAuthenticated);
-    authBypassed = ((_postLaunchActions & SFSDKLaunchActionAuthBypassed) == SFSDKLaunchActionAuthBypassed);
-    XCTAssertFalse(userAuthenticatedAtLaunch, @"User should not generate an authenticated status at launch when bypass is configured.");
-    XCTAssertFalse(userAlreadyAuthenticated, @"User should not generate an already-authenticated status when bypass is configured.");
-    XCTAssertTrue(authBypassed, @"Launch should have generated an auth-bypassed status.");
 }
 
 - (void)testUserSwitching
 {
     [SalesforceSDKManager sharedManager].appConfig.shouldAuthenticate = NO;
-    [self createStandardPostLaunchBlock];
     [self createTestAppIdentity];
     XCTAssertNil([SFUserAccountManager sharedInstance].currentUser, @"Current user should be nil.");
     [[SFUserAccountManager sharedInstance] setCurrentUserInternal:[self createUserAccount]];
@@ -508,47 +364,6 @@ static NSString* const kTestAppName = @"OverridenAppName";
 
 #pragma mark - Private helpers
 
-- (void)createStandardPostLaunchBlock
-{
-    [SalesforceSDKManager sharedManager].postLaunchAction = ^(SFSDKLaunchAction launchActions) {
-        self->_postLaunchBlockCalled = YES;
-        self->_postLaunchActions = launchActions;
-    };
-}
-
-- (void)createStandardLaunchErrorBlock
-{
-    [SalesforceSDKManager sharedManager].launchErrorAction = ^(NSError *error, SFSDKLaunchAction launchAction) {
-        self->_launchError = error;
-        self->_launchErrorBlockCalled = YES;
-    };
-}
-
-- (void)launchAndVerify:(BOOL)launchShouldSucceed failMessage:(NSString *)failMessage
-{
-    _postLaunchActions = SFSDKLaunchActionNone;
-    _postLaunchBlockCalled = NO;
-    _launchErrorBlockCalled = NO;
-    _launchError = nil;
-    BOOL didLaunch = [[SalesforceSDKManager sharedManager] launch];
-    XCTAssertEqual(didLaunch, launchShouldSucceed, @"%@", failMessage);
-}
-
-- (void)verifyPostLaunchState
-{
-    BOOL launchCompleted = [_currentSdkManagerFlow waitForLaunchCompletion];
-    XCTAssertTrue(launchCompleted, @"Launch failed to complete.");
-    XCTAssertTrue(_postLaunchBlockCalled, @"Launch should have gone to post-launch.");
-}
-
-- (void)verifyLaunchErrorState:(BOOL)shouldReachErrorState
-{
-    BOOL launchCompleted = [_currentSdkManagerFlow waitForLaunchCompletion];
-    XCTAssertTrue(launchCompleted, @"Launch failed to complete.");
-    XCTAssertTrue(_launchErrorBlockCalled == shouldReachErrorState, @"Invalid launch error state.");
-    XCTAssertEqual(_launchError != nil, shouldReachErrorState, @"Launch error not consistent with expected launch error state.");
-}
-
 - (void)createTestAppIdentity
 {
     [SalesforceSDKManager sharedManager].appConfig.remoteAccessConsumerKey = @"test_connected_app_id";
@@ -573,24 +388,14 @@ static NSString* const kTestAppName = @"OverridenAppName";
 
 - (void)setupSdkManagerState
 {
-    _currentSdkManagerFlow = [[SFTestSDKManagerFlow alloc] initWithStepTimeDelaySecs:kTimeDelaySecsBetweenLaunchSteps];
+    _currentSdkManagerFlow = [[SFTestSDKManagerFlow alloc] init];
     _origSdkManagerFlow = [SalesforceSDKManager sharedManager].sdkManagerFlow; [SalesforceSDKManager sharedManager].sdkManagerFlow = _currentSdkManagerFlow;
     _origConnectedAppId = [SalesforceSDKManager sharedManager].appConfig.remoteAccessConsumerKey; [SalesforceSDKManager sharedManager].appConfig.remoteAccessConsumerKey = @"";
     _origConnectedAppCallbackUri = [SalesforceSDKManager sharedManager].appConfig.oauthRedirectURI; [SalesforceSDKManager sharedManager].appConfig.oauthRedirectURI = @"";
     _origAuthScopes = [SalesforceSDKManager sharedManager].appConfig.oauthScopes; [SalesforceSDKManager sharedManager].appConfig.oauthScopes = [NSSet set];
     _origAuthenticateAtLaunch = [SalesforceSDKManager sharedManager].appConfig.shouldAuthenticate; [SalesforceSDKManager sharedManager].appConfig.shouldAuthenticate = YES;
-    _origHasVerifiedPasscodeAtStartup = [SalesforceSDKManager sharedManager].hasVerifiedPasscodeAtStartup; [SalesforceSDKManager sharedManager].hasVerifiedPasscodeAtStartup = NO;
-    _origPostLaunchAction = [SalesforceSDKManager sharedManager].postLaunchAction; [SalesforceSDKManager sharedManager].postLaunchAction = NULL;
-    _origLaunchErrorAction = [SalesforceSDKManager sharedManager].launchErrorAction; [SalesforceSDKManager sharedManager].launchErrorAction = NULL;
-    _origPostLogoutAction = [SalesforceSDKManager sharedManager].postLogoutAction; [SalesforceSDKManager sharedManager].postLogoutAction = NULL;
-    _origSwitchUserAction = [SalesforceSDKManager sharedManager].switchUserAction; [SalesforceSDKManager sharedManager].switchUserAction = NULL;
-    _origPostAppForegroundAction = [SalesforceSDKManager sharedManager].postAppForegroundAction; [SalesforceSDKManager sharedManager].postAppForegroundAction = NULL;
     _origCurrentUser = [SFUserAccountManager sharedInstance].currentUser;
     [[SFUserAccountManager sharedInstance] setCurrentUserInternal:nil];
-    _postLaunchBlockCalled = NO;
-    _postLaunchActions = SFSDKLaunchActionNone;
-    _launchErrorBlockCalled = NO;
-    _launchError = nil;
     _origAppName = SalesforceSDKManager.ailtnAppName;
     _origProcessPool = SFSDKWebViewStateManager.sharedProcessPool;
     _origBrandLoginPath = [SalesforceSDKManager sharedManager].brandLoginPath;
@@ -603,12 +408,6 @@ static NSString* const kTestAppName = @"OverridenAppName";
     [SalesforceSDKManager sharedManager].appConfig.oauthRedirectURI = _origConnectedAppCallbackUri;
     [SalesforceSDKManager sharedManager].appConfig.oauthScopes = _origAuthScopes;
     [SalesforceSDKManager sharedManager].appConfig.shouldAuthenticate = _origAuthenticateAtLaunch;
-    [SalesforceSDKManager sharedManager].hasVerifiedPasscodeAtStartup = _origHasVerifiedPasscodeAtStartup;
-    [SalesforceSDKManager sharedManager].postLaunchAction = _origPostLaunchAction;
-    [SalesforceSDKManager sharedManager].launchErrorAction = _origLaunchErrorAction;
-    [SalesforceSDKManager sharedManager].postLogoutAction = _origPostLogoutAction;
-    [SalesforceSDKManager sharedManager].switchUserAction = _origSwitchUserAction;
-    [SalesforceSDKManager sharedManager].postAppForegroundAction = _origPostAppForegroundAction;
     [[SFUserAccountManager sharedInstance] setCurrentUserInternal:_origCurrentUser];
     SalesforceSDKManager.ailtnAppName = _origAppName;
     SFSDKWebViewStateManager.sharedProcessPool = _origProcessPool;
