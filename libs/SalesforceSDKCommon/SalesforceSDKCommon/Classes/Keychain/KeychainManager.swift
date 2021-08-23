7ab802d6cc200e49bc645dbfe84cb121ffedfce9
Raj Rao <rao.r@salesforce.com>
2021-08-23 13:18:33 -0700

    Performance improvements for keychain

--- libs/SalesforceSDKCommon/SalesforceSDKCommon/Classes/Keychain/KeychainManager.swift
+++ libs/SalesforceSDKCommon/SalesforceSDKCommon/Classes/Keychain/KeychainManager.swift
@@ -24,7 +24,6 @@
 //  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
 //  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
 //  WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 import Foundation
 
 internal protocol KeychainItemQueryable {
@@ -109,7 +108,7 @@ internal class KeychainItemManager: NSObject {
         if status != errSecSuccess {
             return KeychainResult(error: KeychainItemManager.mapError(from: status), status: status)
         }
-
+      
         return self.getValue()
     }
 
@@ -267,10 +266,13 @@ public class KeychainHelper: NSObject {
     /// - Returns: KeychainResult
     @objc public class func read(service: String, account: String?) -> KeychainResult {
         self.upgradeIfRequired()
-        let keychainManager = KeychainItemManager(service: service,
-                                                  account: account,
-                                                  accessibilityAttribute: KeychainHelper.keychainAccessibleAttribute)
-        return keychainManager.getValue()
+        return CachedWrapper.wrap(service, account) { service, account in
+            let keychainManager = KeychainItemManager(service: service,
+                                                      account: account,
+                                                      accessibilityAttribute: KeychainHelper.keychainAccessibleAttribute)
+            return keychainManager.getValue()
+        }
+       
     }
 
     /// Create an item in the keychain if not present.
@@ -280,14 +282,19 @@ public class KeychainHelper: NSObject {
     /// - Returns: KeychainResult
     @objc public class func createIfNotPresent(service: String, account: String?) -> KeychainResult {
         self.upgradeIfRequired()
-        let keychainManager = KeychainItemManager(service: service,
-                                                  account: account,
-                                                  accessibilityAttribute: KeychainHelper.keychainAccessibleAttribute)
-        var keychainResult = keychainManager.getValue()
-        if !keychainResult.success && keychainResult.status == errSecItemNotFound {
-            keychainResult = keychainManager.addEmptyValue()
+        
+        return CachedWrapper.wrap(service, account) { service, account in
+            let keychainManager = KeychainItemManager(service: service,
+                                                      account: account,
+                                                      accessibilityAttribute: KeychainHelper.keychainAccessibleAttribute)
+            var keychainResult = keychainManager.getValue()
+            if !keychainResult.success && keychainResult.status == errSecItemNotFound {
+                keychainResult = keychainManager.addEmptyValue()
+            }
+            
+            return keychainResult
         }
-        return keychainResult
+       
     }
 
     /// Write or Update an item in the keychain if not present.
@@ -298,10 +305,14 @@ public class KeychainHelper: NSObject {
     /// - Returns: KeychainResult
     @objc public class func write(service: String, data: Data, account: String?) -> KeychainResult {
         self.upgradeIfRequired()
-        let keychainManager = KeychainItemManager(service: service,
-                                                  account: account,
-                                                  accessibilityAttribute: KeychainHelper.keychainAccessibleAttribute)
-        return keychainManager.setValue(data)
+        
+        return CachedWrapper.wrapWrites(service, data, account) { service, data, account in
+            let keychainManager = KeychainItemManager(service: service,
+                                                      account: account,
+                                                      accessibilityAttribute: KeychainHelper.keychainAccessibleAttribute)
+            return keychainManager.setValue(data)
+        }
+        
     }
 
     /// If an item is found remove it and then add an empty entry i.e. data is nil.
@@ -311,14 +322,17 @@ public class KeychainHelper: NSObject {
     /// - Returns: KeychainResult
     @objc public class func reset(service: String, account: String?) -> KeychainResult {
         self.upgradeIfRequired()
-        let keychainManager = KeychainItemManager(service: service,
-                                                  account: account,
-                                                  accessibilityAttribute: KeychainHelper.keychainAccessibleAttribute)
-        var keychainResult = keychainManager.getValue()
-        if keychainResult.success, keychainManager.removeValue().success {
-            keychainResult = keychainManager.addEmptyValue()
+        
+        return CachedWrapper.wrapRemoves(service, account) { service, account in
+            let keychainManager = KeychainItemManager(service: service,
+                                                      account: account,
+                                                      accessibilityAttribute: KeychainHelper.keychainAccessibleAttribute)
+            var keychainResult = keychainManager.getValue()
+            if keychainResult.success, keychainManager.removeValue().success {
+                keychainResult = keychainManager.addEmptyValue()
+            }
+            return keychainResult
         }
-        return keychainResult
     }
 
     /// Remove an item from the keychain.
@@ -328,41 +342,52 @@ public class KeychainHelper: NSObject {
     /// - Returns: KeychainResult
     @objc public class func remove(service: String, account: String?) -> KeychainResult {
         self.upgradeIfRequired()
-        let keychainManager = KeychainItemManager(service: service,
-                                                  account: account,
-                                                  accessibilityAttribute: KeychainHelper.keychainAccessibleAttribute)
-        var keychainResult = keychainManager.getValue()
-        if keychainResult.success {
-            keychainResult = keychainManager.removeValue()
+        
+        return CachedWrapper.wrapRemoves(service, account) { service, account in
+            let keychainManager = KeychainItemManager(service: service,
+                                                      account: account,
+                                                      accessibilityAttribute: KeychainHelper.keychainAccessibleAttribute)
+            var keychainResult = keychainManager.getValue()
+            if keychainResult.success {
+                keychainResult = keychainManager.removeValue()
+            }
+            return keychainResult
         }
-        return keychainResult
+       
     }
 
     /// Remove all keychain items created by the mobile sdk.
     /// - Returns: KeychainResult
     @objc public class func removeAll() -> KeychainResult {
         self.upgradeIfRequired()
-        let deleteQuery: [String: Any] = [
-            String(kSecClass): kSecClassGenericPassword,
-            String(kSecAttrCreator): String(KeychainItemManager.tag)]
+        return CachedWrapper.wrapRemoveAll {
+            let deleteQuery: [String: Any] = [
+                String(kSecClass): kSecClassGenericPassword,
+                String(kSecAttrCreator): String(KeychainItemManager.tag)]
 
-        let deleteStatus =  SecItemDelete(deleteQuery as CFDictionary)
-        if deleteStatus == errSecSuccess {
+            let deleteStatus =  SecItemDelete(deleteQuery as CFDictionary)
+            if deleteStatus == errSecSuccess {
+                return KeychainResult(data: nil, status: deleteStatus)
+            }
+
+            if deleteStatus == errSecItemNotFound {
+                return KeychainResult(error: KeychainItemManager.mapError(from: deleteStatus), status: deleteStatus)
+            }
             return KeychainResult(data: nil, status: deleteStatus)
         }
-
-        if deleteStatus == errSecItemNotFound {
-            return KeychainResult(error: KeychainItemManager.mapError(from: deleteStatus), status: deleteStatus)
-        }
-        return KeychainResult(data: nil, status: deleteStatus)
-
     }
 
     /// Use this to relax or change the accessibility attribute for keychain items.
     /// - Parameter secAttrAccessible: Should be the accessibility attribute as defined by
     /// - Returns: KeychainResult
     @objc public class func setAccessibleAttribute(_ secAttrAccessible: KeychainItemAccessibility) -> KeychainResult {
-
+       
+        if accessibleAttributeMatches(secAttrAccessible) {
+            return KeychainResult.init(data: Data(), status: errSecSuccess)
+        }
+        
+        
+        CachedWrapper.clearAllCaches()
         let accessibleAttribute = secAttrAccessible.asCFString
         if accessibleAttribute == keychainAccessibleAttribute {
             SalesforceLogger.log(KeychainHelper.self,level: .debug, message: "Attempting to update accessibility attribute for mobilesdk keychain items to the same level, will result in a noop")
@@ -412,10 +437,87 @@ public class KeychainHelper: NSObject {
             SalesforceLogger.log(KeychainHelper.self,level: .error, message: "Attempt to update accessibility attribute for mobilesdk items failed!")
             return KeychainResult(error: KeychainItemManager.mapError(from: readStatus), status: readStatus)
         }
-
         return KeychainResult(data: nil, status: readStatus)
     }
+    
+    /// Use this call to clear caches.
+    @objc public class func clearCaches() {
+        CachedWrapper.clearAllCaches()
+    }
+    
+    private class func accessibleAttributeMatches(_ secAttrAccessible: KeychainItemAccessibility) -> Bool {
+        let query: [String: Any] = [String(kSecClass): String(kSecClassGenericPassword),
+                        String(kSecAttrService): KeychainUpgradeManager.baseAppIdentifierKey,
+                        String(kSecMatchLimit): kSecMatchLimitOne,
+                        String(kSecReturnAttributes): kCFBooleanTrue as Any]
 
+
+        var queryResult: CFTypeRef?
+        let status = SecItemCopyMatching(query as CFDictionary, &queryResult)
+
+        guard errSecSuccess == status else {
+            return false
+        }
+        guard let item = queryResult as? [String: Any],
+              let accessibleAttr = item[String(kSecAttrAccessible)] as? String else {
+            return false
+        }
+
+        return secAttrAccessible.asString ==  accessibleAttr
+    }
+    
+    internal class CachedWrapper {
+        
+        static var cache: SafeMutableDictionary<NSString, KeychainResult> = SafeMutableDictionary<NSString, KeychainResult>()
+        
+        class func key(service: String, account: String?) -> NSString {
+            guard let acc = account else {
+                return NSString(string: "\(service)")
+            }
+            return NSString(string: "\(service)_\(acc)")
+        }
+        
+        class func wrap(_ service: String, _ account: String?, readFunc: (String, String?) -> KeychainResult ) -> KeychainResult {
+            guard let result = cache[key(service: service, account: account)]  else{
+                let keychainResult =  readFunc(service, account)
+                if keychainResult.success {
+                    cache[key(service: service, account: account)]  = keychainResult
+                }
+                return keychainResult
+            }
+            return result
+        }
+        
+        class func wrapWrites(_ service: String, _ data: Data, _ account: String?, writeFunc: (String, Data, String?) -> KeychainResult ) -> KeychainResult {
+            if let _ = cache[key(service: service, account: account)] {
+                cache.removeObject(key(service: service, account: account))
+            }
+            let newKeychainResult = writeFunc(service, data, account)
+            cache[key(service: service, account: account)] = newKeychainResult
+            return newKeychainResult
+        }
+        
+        class func wrapRemoves(_ service: String, _ account: String?, removeFunc: (String, String?) -> KeychainResult ) -> KeychainResult {
+            if let _ = cache[key(service: service, account: account)] {
+                cache.removeObject(key(service: service, account: account))
+            }
+            let newKeychainResult = removeFunc(service, account)
+            return newKeychainResult
+        }
+        
+        class func wrapRemoveAll(removeAllFunc: () -> KeychainResult) -> KeychainResult {
+            cache.removeAllObjects()
+            let newKeychainResult = removeAllFunc()
+            return newKeychainResult
+        }
+        
+        class func clearAllCaches() {
+            self.cache.removeAllObjects()
+        }
+       
+    }
+
+    
     //Pre 9.1 Upgrade Handling
     internal class KeychainUpgradeManager {
 
