46e7aa61f219131306ed811ac7e49670b65293dd
Brandon Page <bpage1@salesforce.com>
2021-09-24 19:10:02 -0700

    Deprecate ActivityMonitor.  Remove custom passcode view from RestAPIExplorer.

--- libs/SalesforceSDKCore/SalesforceSDKCore/Classes/Common/SalesforceSDKManager.m
+++ libs/SalesforceSDKCore/SalesforceSDKCore/Classes/Common/SalesforceSDKManager.m
@@ -841,9 +841,7 @@ void dispatch_once_on_main_thread(dispatch_once_t *predicate, dispatch_block_t b
 - (void)userAccountManager:(SFUserAccountManager *)userAccountManager
          didSwitchFromUser:(SFUserAccount *)fromUser
                     toUser:(SFUserAccount *)toUser
-{
-    // TODO: Deprecate this?  Why is this public API?
-}
+{ }
 
 #pragma mark - SFSecurityLockout
 
