1ad213b2e56f69327aa5847d7df08d6d88a86c11
Brianna Birman <brianna.birman@salesforce.com>
2022-05-25 15:53:38 -0700

    Fix check

--- libs/MobileSync/MobileSyncTests/BriefcaseSyncDownTests.swift
+++ libs/MobileSync/MobileSyncTests/BriefcaseSyncDownTests.swift
@@ -82,7 +82,7 @@ class BriefcaseSyncDownTests: SyncManagerTestCase {
         
         let target = BriefcaseSyncDownTarget(infos: [briefcaseAccountInfo], countIdsPerRetrieve: 2000)
         let briefcaseRecords = try startFetch(target: target, syncManager: syncManager, maxTimestamp: 0)
-        XCTAssertEqual(numberAccounts, briefcaseRecords)
+        XCTAssertEqual(Int(numberAccounts), briefcaseRecords.count)
         
         for briefcaseRecord in briefcaseRecords {
             let id = try XCTUnwrap(briefcaseRecord[ID] as? String)
