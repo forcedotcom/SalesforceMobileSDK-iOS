5e391b598cce27fa2120f7179dff15ef2da6cbc5
Matt Wimmer <mwimmer@salesforce.com>
2022-08-24 16:33:45 -0700

    @W-11296703 - Crash when writing event in analytics event store manager.

--- libs/SalesforceAnalytics/SalesforceAnalytics/Classes/Store/SFSDKEventStoreManager.m
+++ libs/SalesforceAnalytics/SalesforceAnalytics/Classes/Store/SFSDKEventStoreManager.m
@@ -96,14 +96,26 @@
     if (encryptedData) {
         NSString *filename = [self filenameForEvent:eventCopy.eventId];
         NSString *parentDir = [filename stringByDeletingLastPathComponent];
-        [[NSFileManager defaultManager] createDirectoryAtPath:parentDir withIntermediateDirectories:YES attributes: @{ NSFileProtectionKey: NSFileProtectionCompleteUntilFirstUserAuthentication } error:&error];
-        [encryptedData writeToFile:filename options:NSDataWritingFileProtectionCompleteUntilFirstUserAuthentication error:&error];
+        NSFileManager *fileManager = [NSFileManager defaultManager];        
+        [fileManager createDirectoryAtPath:parentDir withIntermediateDirectories:YES attributes: @{ NSFileProtectionKey: NSFileProtectionCompleteUntilFirstUserAuthentication } error:&error];
+        
         if (error) {
-            [SFSDKAnalyticsLogger w:[self class] format:@"Error occurred while writing to file: %@", error.localizedDescription];
-        } else {
-            @synchronized (self.eventCountMutex) {
-                self.numStoredEvents++;
+            [SFSDKAnalyticsLogger w:[self class] format:@"Error occurred while trying to create directory: %@", error.localizedDescription];
+            return;
+        }
+        
+        @try {
+            [encryptedData writeToFile:filename options:NSDataWritingFileProtectionCompleteUntilFirstUserAuthentication error:&error];
+            if (error) {
+                [SFSDKAnalyticsLogger w:[self class] format:@"Error occurred while writing to file: %@", error.localizedDescription];
+            } else {
+                @synchronized (self.eventCountMutex) {
+                    self.numStoredEvents++;
+                }
             }
+        }
+        @catch (NSException *e) {
+            [SFSDKAnalyticsLogger w:[self class] format:@"Exception occurred while writing to file: %@", e.reason];
         }
     }
 }
