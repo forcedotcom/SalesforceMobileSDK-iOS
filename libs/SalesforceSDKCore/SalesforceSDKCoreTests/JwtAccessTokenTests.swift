2a3b4bd6130508d52b751d26dcf9254c8ebbad80
Wolfgang Mathurin <wmathurin@salesforce.com>
2025-03-28 15:47:06 -0700

    Not using real (expired) JWT in JWT parsing test

--- libs/SalesforceSDKCore/SalesforceSDKCoreTests/JwtAccessTokenTests.swift
+++ libs/SalesforceSDKCore/SalesforceSDKCoreTests/JwtAccessTokenTests.swift
@@ -30,8 +30,18 @@ import XCTest
 @testable import SalesforceSDKCore
 
 class JwtAccessTokenTests: XCTestCase {
-    private let testRawJwt = "eyJ0bmsiOiJjb3JlL2ZhbGNvbnRlc3QxLWNvcmU0c2RiNi8wMERTRzAwMDAwOUZZVmQyQU8iLCJ2ZXIiOiIxLjAiLCJraWQiOiJDT1JFX0FUSldULjAwRFNHMDAwMDA5RllWZC4xNzI1NTc5NDIwMTI1IiwidHR5Ijoic2ZkYy1jb3JlLXRva2VuIiwidHlwIjoiSldUIiwiYWxnIjoiUlMyNTYifQ.eyJzY3AiOiJyZWZyZXNoX3Rva2VuIHdlYiBhcGkiLCJhdWQiOlsiaHR0cHM6Ly9tb2JpbGVzZGthdHNkYjYudGVzdDEubXkucGMtcm5kLnNhbGVzZm9yY2UuY29tIl0sInN1YiI6InVpZDowMDVTRzAwMDAwOGVpQWJZQUkiLCJuYmYiOjE3MzAzODY2MjAsIm10eSI6Im9hdXRoIiwic2ZpIjoiYTZjZjk1MjY2NjYzM2Q4ZDUxMDUzNjkzNDcwZDczYzVhOTY4ZTA4NmQ1OGQ2NzlmYTVjMzY1ZmNhMGZiZjhkYyIsInJvbGVzIjpbXSwiaXNzIjoiaHR0cHM6Ly9tb2JpbGVzZGthdHNkYjYudGVzdDEubXkucGMtcm5kLnNhbGVzZm9yY2UuY29tIiwiaHNjIjpmYWxzZSwiZXhwIjoxNzMwMzg2Njk1LCJpYXQiOjE3MzAzODY2MzUsImNsaWVudF9pZCI6IjNNVkc5LkFnd3RvSXZFUlNkOGk4bGVQcnFmczdDYXpSeDJsbGJMOHViTm9HNlIzSHNZb21RRlJwYmF5YU1INEh0ekgzemowTkRFbUMwUElvaHcwUGYifQ.R8RDUDlRD-6LIzV2epi8y7m1_zWBwfvmTAhUiGOjg1fDWGxsX48hSi95WITHtZ-D-gDQEjVl1GBGKsIe7jEBdGkhoFhbUuYFEnd15bcYlmLBIpmRdSbSvImusaeGVBx2hLhv4Icl7md_BuNoiz6BpuV-T_0a0QxRkpo97sGN1MghO6m9ItzXY9ldR7m5_pOORy3eZ1q4JZ1aj49pphom_O_ZQAeWYX7Gp9dZjhxlLFYgk0XrarC689LOhfSAyBhJO-OvtgKrvUY1XiWEaZR3A2FAk-AK1ZrNenKB_76JGEppuODCpRyqiUUlLmFkzcx897KeTQGoC_QDrdn0y4speA"
+    let header = "{\"tnk\":\"some-tnk\",\"ver\":\"1.0\",\"kid\":\"some-kid\",\"tty\":\"sfdc-core-token\",\"typ\":\"JWT\",\"alg\":\"RS256\"}"
+    let payload = "{\"scp\":\"refresh_token web api\",\"aud\":[\"https://mobilesdkatsdb6.test1.my.pc-rnd.salesforce.com\"],\"sub\":\"uid:some-uid\",\"nbf\":1730386620,\"mty\":\"oauth\",\"sfi\":\"some-sfi\",\"roles\":[],\"iss\":\"https://mobilesdkatsdb6.test1.my.pc-rnd.salesforce.com\",\"hsc\":false,\"exp\":1730386695,\"iat\":1730386635,\"client_id\":\"some-client-id\"}"
+    let signature = "FAKE_SIGNATURE"
 
+    var testRawJwt: String {
+        let base64Header = Data(header.utf8).base64EncodedString()
+        let base64Payload = Data(payload.utf8).base64EncodedString()
+        let base64Signature = Data(signature.utf8).base64EncodedString()
+        
+        return [base64Header, base64Payload, base64Signature].joined(separator: ".")
+    }
+    
     func testDecodeValidJwtAndParseHeader() {
         do {
             let decodedJwt = try JwtAccessToken(jwt: testRawJwt)
@@ -42,9 +52,9 @@ class JwtAccessTokenTests: XCTestCase {
 
             XCTAssertEqual(jwtHeader.algorithm, "RS256")
             XCTAssertEqual(jwtHeader.type, "JWT")
-            XCTAssertEqual(jwtHeader.keyId, "CORE_ATJWT.00DSG000009FYVd.1725579420125")
+            XCTAssertEqual(jwtHeader.keyId, "some-kid")
             XCTAssertEqual(jwtHeader.tokenType, "sfdc-core-token")
-            XCTAssertEqual(jwtHeader.tenantKey, "core/falcontest1-core4sdb6/00DSG000009FYVd2AO")
+            XCTAssertEqual(jwtHeader.tenantKey, "some-tnk")
             XCTAssertEqual(jwtHeader.version, "1.0")
         } catch {
             XCTFail("Unexpected error: \(error)")
@@ -63,9 +73,9 @@ class JwtAccessTokenTests: XCTestCase {
             XCTAssertEqual(jwtPayload.expirationTime, 1730386695)
             XCTAssertEqual(jwtPayload.issuer, "https://mobilesdkatsdb6.test1.my.pc-rnd.salesforce.com")
             XCTAssertEqual(jwtPayload.notBeforeTime, 1730386620)
-            XCTAssertEqual(jwtPayload.subject, "uid:005SG000008eiAbYAI")
+            XCTAssertEqual(jwtPayload.subject, "uid:some-uid")
             XCTAssertEqual(jwtPayload.scopes, "refresh_token web api")
-            XCTAssertEqual(jwtPayload.clientId, "3MVG9.AgwtoIvERSd8i8lePrqfs7CazRx2llbL8ubNoG6R3HsYomQFRpbayaMH4HtzH3zj0NDEmC0PIohw0Pf")
+            XCTAssertEqual(jwtPayload.clientId, "some-client-id")
         } catch {
             XCTFail("Unexpected error: \(error)")
         }
