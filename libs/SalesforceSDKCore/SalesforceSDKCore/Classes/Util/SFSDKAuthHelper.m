62144ee16b2bd68e04da9f5e9e6de8ebb3bf6cb9
Riley <crebs@me.com>
2025-05-12 10:51:00 -0600

    Revert "login deeplink hint support"
    This reverts commit d71509a41d900041b8747e533569d2e2940dda26.

--- libs/SalesforceSDKCore/SalesforceSDKCore/Classes/Util/SFSDKAuthHelper.m
+++ libs/SalesforceSDKCore/SalesforceSDKCore/Classes/Util/SFSDKAuthHelper.m
@@ -56,21 +56,6 @@
 }
 
 + (void)loginIfRequired:(UIScene *)scene
-              loginHint:(NSString *)loginHint
-              loginHost:(NSString *)loginHost
-             completion:(void (^)(void))completionBlock
-{
-    [self loginIfRequired:scene
-                loginHint:loginHint
-                loginHost:loginHost
-       frontDoorBridgeUrl:nil
-             codeVerifier:nil
-               completion:completionBlock];
-}
-
-+ (void)loginIfRequired:(UIScene *)scene
-              loginHint:(NSString *)loginHint
-              loginHost:(NSString *)loginHost
      frontDoorBridgeUrl:(NSURL * )frontDoorBridgeUrl
            codeVerifier:(NSString *)codeVerifier
              completion:(void (^)(void))completionBlock
@@ -93,8 +78,6 @@
                        loginWithCompletion:nil
                        failure:failureBlock
                        scene:scene
-                       loginHint:nil
-                       loginHost:nil
                        frontDoorBridgeUrl:frontDoorBridgeUrl
                        codeVerifier:codeVerifier];
         if (!result) {
@@ -103,8 +86,6 @@
                  loginWithCompletion:nil
                  failure:failureBlock
                  scene:scene
-                 loginHint:nil
-                 loginHost:nil
                  frontDoorBridgeUrl:frontDoorBridgeUrl
                  codeVerifier:codeVerifier];
             }];
@@ -112,19 +93,6 @@
     } else {
         [self screenLockValidation:completionBlock];
     }
-}
-
-+ (void)loginIfRequired:(UIScene *)scene
-     frontDoorBridgeUrl:(NSURL * )frontDoorBridgeUrl
-           codeVerifier:(NSString *)codeVerifier
-             completion:(void (^)(void))completionBlock
-{
-    [self loginIfRequired:scene
-                loginHint:nil
-                loginHost:nil
-       frontDoorBridgeUrl:frontDoorBridgeUrl
-             codeVerifier:codeVerifier
-               completion:completionBlock];
 }
 
 +(void)screenLockValidation:(void (^)(void))completionBlock  {
