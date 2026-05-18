38045fd6a938e8fb9cdbd35023a19ec8e1516739
Brianna Birman <brianna.birman@salesforce.com>
2026-05-18 13:19:21 -0700

    Fix hardcoded log level

--- libs/SalesforceSDKCommon/SalesforceSDKCommon/Classes/Logger/SFLogger.m
+++ libs/SalesforceSDKCommon/SalesforceSDKCommon/Classes/Logger/SFLogger.m
@@ -302,7 +302,7 @@ static SFSDKSafeMutableDictionary *loggerList = nil;
 + (void)log:(nonnull Class)cls level:(SFLogLevel)level format:(nonnull NSString *)format, ... {
     va_list args;
     va_start(args, format);
-    [[self defaultLogger] log:cls level:SFLogLevelDefault format:format args:args];
+    [[self defaultLogger] log:cls level:level format:format args:args];
     va_end(args);
 }
 
