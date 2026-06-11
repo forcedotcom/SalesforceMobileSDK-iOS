6b8d6a8375e34da74ec833379c17313c1b9c507b
Takashi Arai <t.arai@salesforce.com>
2026-06-11 09:09:49 -1000

    remove another debug line.

--- native/SampleApps/RestAPIExplorer/RestAPIExplorer/AppDelegate.swift
+++ native/SampleApps/RestAPIExplorer/RestAPIExplorer/AppDelegate.swift
@@ -46,9 +46,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         SalesforceManager.initializeSDK()
         SalesforceManager.shared.appDisplayName = "Rest API Explorer"
-        
-        SalesforceManager.shared.usesDPoP = true
-        
         UserAccountManager.shared.navigationPolicyForAction = { webView, action in
             if let url = action.request.url, url.absoluteString == "https://www.salesforce.com/us/company/privacy" {
                 SFApplicationHelper.open(url, options: [:], completionHandler: nil)
